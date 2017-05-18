cbd579b661ec215a9aa2ce9826920bb38740c0b6
Joost8910 <bluedog@fenria.com>
2017-05-17 23:39:42 -0500

    Update GravitySuitBreastplate.cs

--- Items/equipables/GravitySuitBreastplate.cs
+++ Items/equipables/GravitySuitBreastplate.cs
@@ -8,7 +8,7 @@ using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace MetroidMod
+namespace MetroidMod.Items.equipables
 {
 	public class GravitySuitBreastplate : ModItem
 	{
