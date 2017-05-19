2378dac7b910becc10b940d246a31f2338c4ceff
Joost8910 <bluedog@fenria.com>
2017-05-18 20:15:09 -0500

    Update VariaSuitBreastplate.cs

--- Items/equipables/VariaSuitBreastplate.cs
+++ Items/equipables/VariaSuitBreastplate.cs
@@ -49,16 +49,38 @@ namespace MetroidMod.Items.equipables
 
         public override void UpdateArmorSet(Player p)
         {
-            p.setBonus = "5% increased ranged damage" + "\r\n" + "20% decreased overheat use" + "\r\n" + "Negates fall damage" + "\r\n" + "70% increased underwater breathing";
+            p.setBonus = "Press the Sense move key while moving near an enemy to dodge in that direction" + "\r\n" + "5% increased ranged damage" + "\r\n" + "20% decreased overheat use" + "\r\n" + "Negates fall damage" + "\r\n" + "70% increased underwater breathing";
             p.rangedDamage += 0.05f;
             p.breathMax = (int)(p.breathMax * 1.7f);
             p.noFallDmg = true;
             MPlayer mp = p.GetModPlayer<MPlayer>(mod);
             mp.overheatCost -= 0.20f;
-            mp.enableSenseMove = true;
+             mp.SenseMove(p);
             mp.visorGlow = true;
+            if(!mp.ballstate)
+			{
+				Lighting.AddLight((int)((float)p.Center.X/16f), (int)((float)(p.position.Y+8f)/16f), 0, 0.973f, 0.44f);
+			}
         }
-
+       public override void UpdateVanitySet(Player P)
+		{
+			MPlayer mp = P.GetModPlayer<MPlayer>(mod);
+			mp.isPowerSuit = true;
+			mp.thrusters = true;
+			if(Main.netMode != 2)
+			{
+				mp.thrusterTexture = mod.GetTexture("Gore/powerSuit_thrusters");
+			}
+			mp.visorGlowColor = new Color(0, 248, 112);
+			if(P.velocity.Y != 0f && ((P.controlRight && P.direction == 1) || (P.controlLeft && P.direction == -1)) && mp.shineDirection == 0 && !mp.shineActive && !mp.ballstate)
+			{
+				mp.jet = true;
+			}
+			else if(mp.shineDirection == 0 || mp.shineDirection == 5)
+			{
+				mp.jet = false;
+			}
+		}
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
