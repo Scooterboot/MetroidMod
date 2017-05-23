5abb79f67b36641239651e5920e7aa746abd222e
Joost8910 <bluedog@fenria.com>
2017-05-22 19:07:35 -0500

    Update NovaBeamAddon.cs

--- Items/addons/NovaBeamAddon.cs
+++ Items/addons/NovaBeamAddon.cs
@@ -19,6 +19,13 @@ namespace MetroidMod.Items.addons
 				"+250% overheat use";
 			item.value = 2500;
 			item.rare = 4;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = 1;
+			item.consumable = true;
+			item.createTile = mod.TileType("NovaBeamTile");
 		}
 
 		public override void AddRecipes()
