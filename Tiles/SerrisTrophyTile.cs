bed421b06f3998531be3a7a36b62b563c87081ff
Joost8910 <bluedog@fenria.com>
2017-05-31 20:54:58 -0500

    Add files via upload

---
+++ Tiles/SerrisTrophyTile.cs
@@ -0,0 +1,29 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace MetroidMod.Tiles
+{
+	public class SerrisTrophyTile : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newTile.StyleWrapLimit = 36;
+			TileObjectData.addTile(Type);
+			dustType = 7;
+			disableSmartCursor = true;
+			AddMapEntry(new Color(137, 98, 69), "Serris Trophy");
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Item.NewItem(i * 16, j * 16, 48, 48, mod.ItemType("SerrisTrophy"));
+	
+		}
+	}
+}
