999e82b333c8dbebddf6a7e3278a8b9eb8c94e88
Scooterboot <scooterboot2012@gmail.com>
2017-06-05 16:29:37 -0500

    Update ChoziteGreaves.cs

--- Items/equipables/ChoziteGreaves.cs
+++ Items/equipables/ChoziteGreaves.cs
@@ -4,7 +4,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
 
-namespace Terraria_Metroidmod_TmodLoader.Items.equipables
+namespace MetroidMod.Items.equipables
 {
     class ChoziteGreaves : ModItem
     {
