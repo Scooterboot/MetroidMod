b24af3e350a78144da59ff83213942db2880d7c1
Scooterboot <scooterboot2012@gmail.com>
2017-06-05 13:57:37 -0500

    Update MetroidMod.cs

--- MetroidMod.cs
+++ MetroidMod.cs
@@ -8,6 +8,17 @@ using Terraria.ModLoader;
 namespace MetroidMod {
 public class MetroidMod : Mod
 {
+		public static Color powColor = new Color(248, 248, 110);
+		public static Color iceColor = new Color(0, 255, 255);
+		public static Color waveColor = new Color(215, 0, 215);
+		public static Color waveColor2 = new Color(239, 153, 239);
+		public static Color plaRedColor = new Color(253, 221, 3);
+		public static Color plaGreenColor = new Color(0, 248, 112);
+		public static Color plaGreenColor2 = new Color(61, 248, 154);
+		public static Color novColor = new Color(50, 255, 1);
+		public static Color wideColor = new Color(255, 210, 255);
+
+
 		internal static ModHotKey MorphBallKey;
 		internal static ModHotKey SpiderBallKey;
 		internal static ModHotKey BoostBallKey;
