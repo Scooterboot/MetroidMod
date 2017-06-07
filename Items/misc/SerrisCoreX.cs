ab1dbe6f2d773aa66746b8025b7cadd814239e56
Joost8910 <bluedog@fenria.com>
2017-06-07 18:41:47 -0500

    Add files via upload

--- Items/misc/SerrisCoreX.cs
+++ Items/misc/SerrisCoreX.cs
@@ -10,21 +10,21 @@ namespace MetroidMod.Items.misc
 {
     public class SerrisCoreX : ModItem
     {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Serris Core-X");
+			Tooltip.SetDefault("Soft and squishy\n" + 
+			"Seems to react to kinetic energy, and amplifies it");
+			ItemID.Sets.ItemNoGravity[item.type] = true;
+			Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(5, 8));
+		}
 		public override void SetDefaults()
 		{
-			item.name = "Serris Core-X";
 			item.maxStack = 99;
-			item.toolTip = "Soft and squishy\n" + 
-			"Seems to react to kinetic energy, and amplifies it";
 			item.width = 64;
 			item.height = 64;
 			item.value = 10000;
 			item.rare = 5;
-			ItemID.Sets.ItemNoGravity[item.type] = true;
-		}
-        public override DrawAnimation GetAnimation()
-		{
-			return new DrawAnimationVertical(5, 8);
 		}
 	}
 }
