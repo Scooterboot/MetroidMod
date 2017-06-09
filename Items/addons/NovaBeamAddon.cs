6811578f37ab28edb096ba10ce2a9b53a3ff5049
Scooterboot <scooterboot2012@gmail.com>
2017-06-09 11:06:46 -0500

    Create NovaBeamAddon.cs

--- Items/addons/NovaBeamAddon.cs
+++ Items/addons/NovaBeamAddon.cs
@@ -10,7 +10,7 @@ namespace MetroidMod.Items.addons
 			DisplayName.SetDefault("Nova Beam");
 			Tooltip.SetDefault("Power Beam Addon\n" +
 			"Slot Type: Primary B\n" + 
-				"Shots pierce enemies, hitting them multiple times\n" + 
+				"Shots pierce enemies\n" + 
 				"Shots set enemies ablaze with Cursed Fire, or Frost Burns them if Ice Beam is installed\n" + 
 				"+500% damage\n" + 
 				"+250% overheat use");
@@ -29,25 +29,27 @@ namespace MetroidMod.Items.addons
 			item.useStyle = 1;
 			item.consumable = true;
 			item.createTile = mod.TileType("NovaBeamTile");
+			MGlobalItem mItem = item.GetGlobalItem<MGlobalItem>(mod);
+			mItem.addonSlotType = 4;
 		}
 
 		public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
 			recipe.AddIngredient(null, "PlasmaBeamGreenAddon");
-            recipe.AddIngredient(ItemID.ChlorophyteBar, 10);
-            recipe.AddIngredient(ItemID.Emerald, 5);
+            		recipe.AddIngredient(ItemID.ChlorophyteBar, 10);
+            		recipe.AddIngredient(ItemID.Emerald, 5);
 			recipe.AddIngredient(ItemID.SoulofSight, 5);
-            recipe.AddTile(TileID.MythrilAnvil);
+            		recipe.AddTile(TileID.MythrilAnvil);
 			recipe.SetResult(this);
 			recipe.AddRecipe();
 
 			recipe = new ModRecipe(mod);
 			recipe.AddIngredient(null, "PlasmaBeamRedAddon");
-            recipe.AddIngredient(ItemID.ChlorophyteBar, 10);
-            recipe.AddIngredient(ItemID.Emerald, 5);
+            		recipe.AddIngredient(ItemID.ChlorophyteBar, 10);
+            		recipe.AddIngredient(ItemID.Emerald, 5);
 			recipe.AddIngredient(ItemID.SoulofSight, 5);
-            recipe.AddTile(TileID.MythrilAnvil);
+		    	recipe.AddTile(TileID.MythrilAnvil);
 			recipe.SetResult(this);
 			recipe.AddRecipe();
 		}
