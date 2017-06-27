4dadb90ab62d9ea3a98c25d0d57174205df32581
Joost8910 <bluedog@fenria.com>
2017-06-27 17:34:09 -0500

    Add files via upload

---
+++ NPCs/MNPC.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MetroidMod.NPCs
+{
+	public class MNPC : GlobalNPC 
+	{
+		public override void NPCLoot(NPC npc)
+		{
+			Player player = Main.player[npc.lastInteraction];
+            MPlayer mp = player.GetModPlayer<MPlayer>(mod);
+            for(int i = 0; i < player.inventory.Length; i++)
+			{
+				if(player.inventory[i].type == mod.ItemType("MissileLauncher"))
+				{
+                    if (Main.rand.Next(5) <= 1)
+                    {
+					Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("MissilePickup"), 1 + Main.rand.Next(5));
+                    }
+                    else if (Main.rand.Next(10) == 0)
+                    {
+                        Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("MissilePickup"), 10 + Main.rand.Next(16));
+                    }
+				}
+			}
+
+        }
+	}
+}
+
