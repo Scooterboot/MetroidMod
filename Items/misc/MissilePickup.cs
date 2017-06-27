1a34554620ef69d6e00ddacec404e24ed0bc4a2b
Scooterboot <scooterboot2012@gmail.com>
2017-06-27 13:10:53 -0500

    Add files via upload

---
+++ Items/misc/MissilePickup.cs
@@ -0,0 +1,40 @@
+using System;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace MetroidMod.Items.misc
+{
+    public class MissilePickup : ModItem
+    {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Missile");
+			ItemID.Sets.ItemNoGravity[item.type] = true;
+			Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(6, 2));
+		}
+		public override void SetDefaults()
+		{
+			item.maxStack = 255;
+			item.width = 30;
+			item.height = 30;
+			item.value = 100;
+			item.rare = 1;
+		}
+		public override bool OnPickup(Player player)
+		{
+			for(int i = 0; i < player.inventory.Length; i++)
+			{
+				if(player.inventory[i].type == mod.ItemType("MissileLauncher"))
+				{
+					MGlobalItem mi = player.inventory[i].GetGlobalItem<MGlobalItem>(mod);
+					mi.statMissiles += item.stack;
+				}
+			}
+			return false;
+		}
+	}
+}
