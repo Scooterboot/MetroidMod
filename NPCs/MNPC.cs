1d9b1d0999bb0502c7243bd53e225ceb19424130
Scooterboot <scooterboot2012@gmail.com>
2017-08-01 18:57:17 -0500

    Update MNPC.cs

--- NPCs/MNPC.cs
+++ NPCs/MNPC.cs
@@ -6,32 +6,35 @@ using Terraria.ModLoader;
 
 namespace MetroidMod.NPCs
 {
-	public override void NPCLoot(NPC npc)
+	public class MNPC : GlobalNPC 
 	{
-		Player player = Main.player[(int)Player.FindClosest(npc.position, npc.width, npc.height)];
-    		MPlayer mp = player.GetModPlayer<MPlayer>(mod);
-		bool flag = false;
-    		for(int i = 0; i < player.inventory.Length; i++)
+		public override void NPCLoot(NPC npc)
 		{
-			if(player.inventory[i].type == mod.ItemType("MissileLauncher"))
+			Player player = Main.player[(int)Player.FindClosest(npc.position, npc.width, npc.height)];
+            		MPlayer mp = player.GetModPlayer<MPlayer>(mod);
+			bool flag = false;
+            		for(int i = 0; i < player.inventory.Length; i++)
 			{
-				flag = true;
-			}
-		}
-		if(flag)
-		{
-			if (npc.type != 16 && npc.type != 81 && npc.type != 121 && npc.lifeMax > 1 && npc.damage > 0)
-			{
-				if (Main.rand.Next(5) <= 1)
+				if(player.inventory[i].type == mod.ItemType("MissileLauncher"))
 				{
-					Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("MissilePickup"), 1 + Main.rand.Next(5));
-				}
-				else if (Main.rand.Next(10) == 0)
-				{
-					Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("MissilePickup"), 10 + Main.rand.Next(16));
+					flag = true;
 				}
 			}
-		}
-        }
+			if(flag)
+			{
+				if (npc.type != 16 && npc.type != 81 && npc.type != 121 && npc.lifeMax > 1 && npc.damage > 0)
+				{
+					if (Main.rand.Next(5) <= 1)
+					{
+						Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("MissilePickup"), 1 + Main.rand.Next(5));
+					}
+					else if (Main.rand.Next(10) == 0)
+					{
+						Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("MissilePickup"), 10 + Main.rand.Next(16));
+					}
+				}
+			}
+        	}
+	}
 }
 
