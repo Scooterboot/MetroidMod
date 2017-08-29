d73c2e80d0c74b5e49a0a2e9975829f6df25116c
Scooterboot <scooterboot2012@gmail.com>
2017-08-29 12:24:43 -0500

    Update MorphBallV2.cs

--- Items/equipables/MorphBallV2.cs
+++ Items/equipables/MorphBallV2.cs
@@ -65,14 +65,14 @@ namespace MetroidMod.Items.equipables
         {
 			Player player = Main.player[item.owner];
 			MPlayer mp = player.GetModPlayer<MPlayer>(mod);
-            return mp.currentMorphColor;
+            return mp.morphItemColor;
         }
 		public override void PostDrawInInventory(SpriteBatch sb, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale )
 		{
 			Player player = Main.player[item.owner];
 			MPlayer mp = player.GetModPlayer<MPlayer>(mod);
-			itemColor = mp.currentMorphColor;
-			drawColor = mp.currentMorphColor2;
+			itemColor = mp.morphItemColor;
+			drawColor = mp.morphColorLights;
 			Texture2D tex = mod.GetTexture("Items/equipables/MorphBallV2_Lights");
 			sb.Draw(tex, position, frame, drawColor, 0f, origin, scale, SpriteEffects.None, 0f);
 			sb.Draw(mod.GetTexture("Gore/Spiderball"), position, frame, drawColor, 0f, origin, scale, SpriteEffects.None, 0f);
@@ -82,7 +82,7 @@ public override void PostDrawInWorld(SpriteBatch sb, Color lightColor, Color alp
 			DrawLights(sb, Main.player[item.owner]);
 			DrawSpider(sb, Main.player[item.owner]);
 			MPlayer mp = Main.player[item.owner].GetModPlayer<MPlayer>(mod);
-			lightColor = mp.currentMorphColor2;
+			lightColor = mp.morphColorLights;
 			alphaColor = lightColor;
 		}
 		public void DrawLights(SpriteBatch sb, Player player)
@@ -93,7 +93,7 @@ public override void PostDrawInWorld(SpriteBatch sb, Color lightColor, Color alp
 			float num4 = (float)(item.height - tex.Height);
 			float num5 = (float)(item.width / 2 - tex.Width / 2);
 			MPlayer mp = player.GetModPlayer<MPlayer>(mod);
-			sb.Draw(tex, new Vector2(item.position.X - Main.screenPosition.X + (float)(tex.Width / 2) + num5, item.position.Y - Main.screenPosition.Y + (float)(tex.Height / 2) + num4 + 2f), new Rectangle?(new Rectangle(0, 0, tex.Width, tex.Height)), mp.currentMorphColor2, rotation, new Vector2((float)(tex.Width / 2), (float)(tex.Height / 2)), num3, SpriteEffects.None, 0f);
+			sb.Draw(tex, new Vector2(item.position.X - Main.screenPosition.X + (float)(tex.Width / 2) + num5, item.position.Y - Main.screenPosition.Y + (float)(tex.Height / 2) + num4 + 2f), new Rectangle?(new Rectangle(0, 0, tex.Width, tex.Height)), mp.morphColorLights, rotation, new Vector2((float)(tex.Width / 2), (float)(tex.Height / 2)), num3, SpriteEffects.None, 0f);
 		}
 		public void DrawSpider(SpriteBatch sb, Player player)
 		{
@@ -103,7 +103,7 @@ public override void PostDrawInWorld(SpriteBatch sb, Color lightColor, Color alp
 			float num4 = (float)(item.height - tex.Height);
 			float num5 = (float)(item.width / 2 - tex.Width / 2);
 			MPlayer mp = player.GetModPlayer<MPlayer>(mod);
-			sb.Draw(tex, new Vector2(item.position.X - Main.screenPosition.X + (float)(tex.Width / 2) + num5, item.position.Y - Main.screenPosition.Y + (float)(tex.Height / 2) + num4 + 2f), new Rectangle?(new Rectangle(0, 0, tex.Width, tex.Height)), mp.currentMorphColor2, rotation, new Vector2((float)(tex.Width / 2), (float)(tex.Height / 2)), num3, SpriteEffects.None, 0f);
+			sb.Draw(tex, new Vector2(item.position.X - Main.screenPosition.X + (float)(tex.Width / 2) + num5, item.position.Y - Main.screenPosition.Y + (float)(tex.Height / 2) + num4 + 2f), new Rectangle?(new Rectangle(0, 0, tex.Width, tex.Height)), mp.morphColorLights, rotation, new Vector2((float)(tex.Width / 2), (float)(tex.Height / 2)), num3, SpriteEffects.None, 0f);
 		}
 			public override bool CanEquipAccessory(Player player, int slot)
 		{
