11fb0e336f2011020aefac966b75e406ebca1d0f
Joost8910 <bluedog@fenria.com>
2017-08-30 17:36:40 -0500

    Update TEBlueHatch.cs

--- Tiles/TEBlueHatch.cs
+++ Tiles/TEBlueHatch.cs
@@ -27,7 +27,7 @@ namespace MetroidMod.Tiles
 				float px = Math.Abs((p.Center.X / 16) - (x +1.5f));
 				float py = Math.Abs((p.Center.Y / 16) - (y +1.5f));
 				//Rectangle hitbox = new Rectangle(x*16 - 8, y*16 - 8, (x+3)*16 + 8, (y+3)*16 + 8);
-				if (p.aiStyle != 26 && !p.minion && p.friendly && p.active && /*p.Hitbox.Intersects(hitbox)*/ px < (2.5f + p.width/32) && py < (2.5f + p.width/32))
+				if (p.aiStyle != 26 && !p.minion && p.damage > 0 && p.friendly && p.active && /*p.Hitbox.Intersects(hitbox)*/ px < (2.5f + p.width/32) && py < (2.5f + p.width/32))
 				{
 					//Main.NewText("px is " + px);
 					//Main.NewText("py is " + py);
