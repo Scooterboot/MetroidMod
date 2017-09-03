d95373fd0d0f79375586e3682feb7effe3f0843d
Jeffcole1 <Jeffrey.Coleman.comp@gmail.com>
2017-09-03 19:36:39 -0400

    Remove vanity effects from Chozite Armor

--- Items/equipables/ChoziteBreastplate.cs
+++ Items/equipables/ChoziteBreastplate.cs
@@ -37,26 +37,6 @@ namespace MetroidMod.Items.equipables
             mp.visorGlow = true;
         }
 
-        public override void UpdateVanitySet(Player P)
-        {
-            MPlayer mp = P.GetModPlayer<MPlayer>(mod);
-            mp.isPowerSuit = true;
-            mp.thrusters = true;
-            if (Main.netMode != 2)
-            {
-                mp.thrusterTexture = mod.GetTexture("Gore/powerSuit_thrusters");
-            }
-            mp.visorGlowColor = new Color(0, 248, 112);
-            if (P.velocity.Y != 0f && ((P.controlRight && P.direction == 1) || (P.controlLeft && P.direction == -1)) && mp.shineDirection == 0 && !mp.shineActive && !mp.ballstate)
-            {
-                mp.jet = true;
-            }
-            else if (mp.shineDirection == 0 || mp.shineDirection == 5)
-            {
-                mp.jet = false;
-            }
-        }
-
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
