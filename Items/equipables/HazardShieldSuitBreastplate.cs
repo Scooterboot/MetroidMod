c8a36edb7e22bac81a5aafc5a9705d0f56f45f15
Scooterboot <scooterboot2012@gmail.com>
2017-10-11 10:45:52 -0500

    Update HazardShieldSuitBreastplate.cs

--- Items/equipables/HazardShieldSuitBreastplate.cs
+++ Items/equipables/HazardShieldSuitBreastplate.cs
@@ -95,7 +95,7 @@ namespace MetroidMod.Items.equipables
             recipe.AddIngredient(ItemID.ShroomiteBar, 25);
             //recipe.AddIngredient(null, "", 10);
             recipe.AddIngredient(null, "EnergyTank");
-            recipe.AddTile(null, "NovaWorkTable");
+            recipe.AddTile(null, "NovaWorkTableTile");
             recipe.SetResult(this);
             recipe.AddRecipe();
         }
