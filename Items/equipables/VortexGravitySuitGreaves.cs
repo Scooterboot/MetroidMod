d42be127170e2fa3b65023fb9fdc5737a6df926b
Jeffcole1 <Jeffrey.Coleman.comp@gmail.com>
2017-10-10 22:12:54 -0400

    Add Vortex Gravity Suit

---
+++ Items/equipables/VortexGravitySuitGreaves.cs
@@ -0,0 +1,49 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MetroidMod.Items.equipables
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class VortexGravitySuitGreaves : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Vortex Gravity Suit Greaves");
+            Tooltip.SetDefault("5% increased ranged damage\n" +
+            "20% increased movement speed\n" +
+            "+33 overheat capacity\n" +
+            "Allows you to cling to walls");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 18;
+            item.height = 18;
+            item.rare = 10;
+            item.value = 48000;
+            item.defense = 20;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.rangedDamage += 0.05f;
+            player.moveSpeed += 0.20f;
+            player.spikedBoots += 2;
+            MPlayer mp = player.GetModPlayer<MPlayer>(mod);
+            mp.maxOverheat += 33;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "TerraGravitySuitGreaves");
+            recipe.AddIngredient(ItemID.LunarBar, 20);
+            recipe.AddIngredient(ItemID.FragmentVortex, 10);
+            recipe.AddIngredient(null, "EnergyTank");
+            recipe.AddTile(null, "NovaWorkTable");
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
