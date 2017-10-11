49548580fb2fd89426ce0dbbc244d89ba729bbd9
Scooterboot <scooterboot2012@gmail.com>
2017-10-11 11:50:45 -0500

    Update VortexGravitySuitBreastplate.cs

--- Items/equipables/VortexGravitySuitBreastplate.cs
+++ Items/equipables/VortexGravitySuitBreastplate.cs
@@ -88,6 +88,11 @@ namespace MetroidMod.Items.equipables
                 mp.jet = false;
             }
         }
+		
+		public override void ArmorSetShadows(Player player)
+		{
+			player.armorEffectDrawShadow = true;
+		}
 
         public override void AddRecipes()
         {
