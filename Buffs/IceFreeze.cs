d60524fe2cb94fa3f87401fbfc12d26dec9a4b4f
Joost8910 <bluedog@fenria.com>
2018-01-22 17:03:26 -0600

    Update IceFreeze.cs

--- Buffs/IceFreeze.cs
+++ Buffs/IceFreeze.cs
@@ -18,8 +18,8 @@ namespace MetroidMod.Buffs
 		}
 		public override bool ReApply(NPC N, int time, int buffIndex)
 		{
-			isSkeletronArm = (N.aiStyle == 12 || N.aiStyle == 33 || N.aiStyle == 34 || N.aiStyle == 35 || N.aiStyle == 36);
-			canFreeze = (!N.dontTakeDamage && !N.boss && N.lifeMax < 3000 && !isSkeletronArm &&	N.type != 143 && N.type != 144 && N.type != 145 && N.type != 146 && N.aiStyle != 6 && !N.buffImmune[44]);
+			bool isSkeletronArm = (N.aiStyle == 12 || N.aiStyle == 33 || N.aiStyle == 34 || N.aiStyle == 35 || N.aiStyle == 36);
+			bool canFreeze = (!N.dontTakeDamage && !N.boss && N.lifeMax < 3000 && !isSkeletronArm &&	N.type != 143 && N.type != 144 && N.type != 145 && N.type != 146 && N.aiStyle != 6 && !N.buffImmune[44]);
 			if(canFreeze)
 			{
 				if( N.GetGlobalNPC<NPCs.MNPC>(mod).speedDecrease > 0)
@@ -41,8 +41,8 @@ namespace MetroidMod.Buffs
 		}
 		public override void Update(NPC N,ref int buffIndex)
 		{
-			isSkeletronArm = (N.aiStyle == 12 || N.aiStyle == 33 || N.aiStyle == 34 || N.aiStyle == 35 || N.aiStyle == 36);
-			canFreeze = (!N.dontTakeDamage && !N.boss && N.lifeMax < 3000 && !isSkeletronArm &&	N.type != 143 && N.type != 144 && N.type != 145 && N.type != 146 && N.aiStyle != 6 && !N.buffImmune[44]);
+			bool isSkeletronArm = (N.aiStyle == 12 || N.aiStyle == 33 || N.aiStyle == 34 || N.aiStyle == 35 || N.aiStyle == 36);
+			bool canFreeze = (!N.dontTakeDamage && !N.boss && N.lifeMax < 3000 && !isSkeletronArm &&	N.type != 143 && N.type != 144 && N.type != 145 && N.type != 146 && N.aiStyle != 6 && !N.buffImmune[44]);
 			if(canFreeze)
 			{
 				N.GetGlobalNPC<NPCs.MNPC>(mod).froze = true;
