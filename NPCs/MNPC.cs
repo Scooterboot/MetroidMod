7777510799836f4ce7586353e78a8ff65ae7d727
Joost8910 <bluedog@fenria.com>
2018-01-22 17:05:49 -0600

    Update MNPC.cs

--- NPCs/MNPC.cs
+++ NPCs/MNPC.cs
@@ -8,6 +8,77 @@ namespace MetroidMod.NPCs
 {
 	public class MNPC : GlobalNPC 
 	{
+		public override bool InstancePerEntity
+		{
+		    get
+		    {
+			return true;
+		    }
+		}
+		public bool froze = false;
+		public float speedDecrease = 0.8f;
+		public bool oldGravity;
+		public bool oldTileCollide;
+		int oldDir = 1;
+		bool start = false;
+		public override void ResetEffects(NPC npc)
+		{
+		    froze = false;
+		}
+		public override bool PreAI(NPC npc)
+		{
+		    if (!start)
+		    {
+			oldGravity = npc.noGravity;
+			oldTileCollide = npc.noTileCollide;
+			start = true;
+		    }
+		    if (froze)
+		    {
+			if (speedDecrease <= 0 && npc.type != mod.NPCType("LarvalMetroid"))
+			{
+			    npc.damage = 0;
+			    npc.frame.Y = 0;
+			    npc.velocity.X = 0;
+			    npc.noGravity = false;
+			    npc.noTileCollide = false;
+			    npc.direction = oldDir;
+			    npc.spriteDirection = oldDir;
+			    return false;
+			}
+			else
+			{
+			    oldDir = npc.direction;
+			}
+		    }
+		    else
+		    {
+			speedDecrease = 0.8f;
+			npc.noGravity = oldGravity;
+			npc.noTileCollide = oldTileCollide;
+		    }
+		    return base.PreAI(npc);
+		}
+		public override void PostAI(NPC npc)
+		{
+		    base.PostAI(npc);
+		    if (froze && speedDecrease > 0 && npc.type != mod.NPCType("LarvalMetroid"))
+		    {
+			npc.velocity.X = npc.velocity.X * speedDecrease;
+			if (npc.noGravity)
+			{
+			    npc.velocity.Y = npc.velocity.Y * speedDecrease;
+			}
+
+		    }
+		}
+		public override void DrawEffects(NPC npc, ref Color drawColor)
+		{
+		    if (froze)
+		    {
+			drawColor = Lighting.GetColor((int)npc.position.X / 16, (int)npc.position.Y / 16, new Color(0, 144, 255));
+		    }
+		}
 		public override void NPCLoot(NPC npc)
 		{
 			Player player = Main.player[(int)Player.FindClosest(npc.position, npc.width, npc.height)];
