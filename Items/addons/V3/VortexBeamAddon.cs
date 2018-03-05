44dbf0b4cb71549c452360245bfb016ac18fc866
Joost8910 <bluedog@fenria.com>
2018-03-05 17:21:49 -0600

    Update VortexBeamAddon.cs

--- Items/addons/V3/VortexBeamAddon.cs
+++ Items/addons/V3/VortexBeamAddon.cs
@@ -23,13 +23,13 @@ namespace MetroidMod.Items.addons.V3
 			item.maxStack = 1;
 			item.value = 2500;
 			item.rare = 4;
-			/*item.useTurn = true;
+			item.useTurn = true;
 			item.autoReuse = true;
 			item.useAnimation = 15;
 			item.useTime = 10;
 			item.useStyle = 1;
 			item.consumable = true;
-			item.createTile = mod.TileType("VortexBeamTile");*/
+			item.createTile = mod.TileType("VortexBeamTile");
 			MGlobalItem mItem = item.GetGlobalItem<MGlobalItem>(mod);
 			mItem.addonSlotType = 3;
 		}
