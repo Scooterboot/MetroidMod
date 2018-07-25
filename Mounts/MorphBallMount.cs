77a0e777c8cae08fbbe7ca055d1441d1f72454c1
Scooterboot <scooterboot2012@gmail.com>
2018-07-25 14:37:23 -0500

    Update MorphBallMount.cs

--- Mounts/MorphBallMount.cs
+++ Mounts/MorphBallMount.cs
@@ -22,7 +22,7 @@ namespace MetroidMod.Mounts
 			mountData.spawnDust = 299;//63;
 			mountData.spawnDustNoGravity = true;
 			mountData.buff = mod.BuffType("MorphBallMountBuff");
-			mountData.heightBoost = -(42 - MPlayer.morphSize);
+			mountData.heightBoost = 0;//-(42 - MPlayer.morphSize);
 			
 			mountData.runSpeed = 3f;
 			mountData.acceleration = 0.08f;
@@ -41,87 +41,68 @@ namespace MetroidMod.Mounts
 			int[] array = new int[mountData.totalFrames];
 			mountData.playerYOffsets = array;
 			mountData.playerHeadOffset = mountData.heightBoost;
-			/*if (Main.netMode != 2)
-			{
-				mountData.textureWidth = mountData.backTexture.Width + 20;
-				mountData.textureHeight = mountData.backTexture.Height;
-			}*/
 		}
 
+		Item[] slotItem = new Item[5];
 		public override void UpdateEffects(Player player)
 		{
-			/*mountData.runSpeed = 3f;
-			mountData.acceleration = 0.08f;
-			mountData.dashSpeed = 3f;
-			mountData.acceleration *= player.moveSpeed;
-			mountData.runSpeed *= player.moveSpeed;
-			
-			mountData.jumpHeight = 15;
-			mountData.jumpSpeed = 5.01f;
-			if (player.jumpBoost)
-			{
-				mountData.jumpHeight = 20;
-				mountData.jumpSpeed = 6.51f;
-			}
-			if (player.wereWolf)
-			{
-				mountData.jumpHeight += 2;
-				mountData.jumpSpeed += 0.2f;
-			}
-			mountData.jumpSpeed += player.jumpSpeedBoost;*/
-			
 			MPlayer mp = player.GetModPlayer<MPlayer>(mod);
 			
-			MorphBall mBall = (MorphBall)player.miscEquips[3].modItem;
-			if(mBall != null)
+			if(player.miscEquips[3].type == mod.ItemType("MorphBall"))
 			{
-				BallUI ballUI = mBall.ballUI;
-				
-				int pb = mod.ItemType("PowerBombAddon");
-				int sb = mod.ItemType("SpiderBallAddon");
-				int bb = mod.ItemType("BoostBallAddon");
+				MorphBall mBall = (MorphBall)player.miscEquips[3].modItem;
+				if(mBall != null)
+				{
+					BallUI ballUI = mBall.ballUI;
+					
+					for(int i = 0; i < 5; i++)
+					{
+						slotItem[i] = ballUI.ballSlot[i].item;
+					}
+				}
+			}
+			int pb = mod.ItemType("PowerBombAddon");
+			int sb = mod.ItemType("SpiderBallAddon");
+			int bb = mod.ItemType("BoostBallAddon");
 
-				Item slotDrill = ballUI.ballSlot[0].item;
-				Item slotBomb = ballUI.ballSlot[1].item;
-				Item slotSpecial = ballUI.ballSlot[2].item;
-				Item slotUtility = ballUI.ballSlot[3].item;
-				Item slotBoost = ballUI.ballSlot[4].item;
-
-
-				mp.morphBall = true;
-				mp.MorphBallBasic(player);
-				if (!slotDrill.IsAir)
-				{
-					MGlobalItem drillMItem = slotDrill.GetGlobalItem<MGlobalItem>(mod);
-					mp.Drill(player,drillMItem.drillPower);
-				}
-				if (!slotBomb.IsAir)
-				{
-					MGlobalItem bombMItem = slotBomb.GetGlobalItem<MGlobalItem>(mod);
-					mp.bombDamage = (int)(player.rangedDamage * bombMItem.bombDamage);
-					mp.Bomb(player);
-				}
-				if (slotSpecial.type == pb)
-				{
-					mp.PowerBomb(player);
-				}
-				if (slotUtility.type == sb)
-				{
-					mp.SpiderBall(player);
-				}
-				else
-				{
-					mp.spiderball = false;
-				}
-				if (slotBoost.type == bb)
-				{
-					mp.BoostBall(player);
-				}
-				else
-				{
-					mp.boostCharge = 0;
-					mp.boostEffect = 0;
-				}
+			mp.morphBall = true;
+			mp.MorphBallBasic(player);
+			if (slotItem[0] != null && !slotItem[0].IsAir)
+			{
+				MGlobalItem drillMItem = slotItem[0].GetGlobalItem<MGlobalItem>(mod);
+				mp.Drill(player,drillMItem.drillPower);
+			}
+			if (slotItem[1] != null && !slotItem[1].IsAir)
+			{
+				MGlobalItem bombMItem = slotItem[1].GetGlobalItem<MGlobalItem>(mod);
+				mp.bombDamage = (int)(player.rangedDamage * bombMItem.bombDamage);
+				mp.Bomb(player);
+			}
+			if (slotItem[2] != null && slotItem[2].type == pb)
+			{
+				mp.PowerBomb(player);
+			}
+			if (slotItem[3] != null && slotItem[3].type == sb)
+			{
+				mp.SpiderBall(player);
+			}
+			else
+			{
+				mp.spiderball = false;
+			}
+			if (slotItem[4] != null && slotItem[4].type == bb)
+			{
+				mp.BoostBall(player);
+			}
+			else
+			{
+				mp.boostCharge = 0;
+				mp.boostEffect = 0;
+			}
+			
+			for(int i = 0; i < 5; i++)
+			{
+				slotItem[i] = null;
 			}
 		}
 	}
