f0ef742c8ce8ab7e0f67ae38b2adb9007ea195f4
Scooterboot <scooterboot2012@gmail.com>
2018-10-31 17:25:50 -0500

    Update MGlobalProjectile.cs

--- Projectiles/MGlobalProjectile.cs
+++ Projectiles/MGlobalProjectile.cs
@@ -17,6 +17,13 @@ namespace MetroidMod.Projectiles
 {
     public class MGlobalProjectile : GlobalProjectile
     {
+		public override bool InstancePerEntity
+		{
+			get
+			{
+				return true;
+			}
+		}
         /*public override bool OnTileCollide(Projectile projectile, Vector2 oldVelocity)
         {
             if (projectile.aiStyle != 26 && !projectile.minion && projectile.damage > 0 && projectile.friendly && projectile.active)
@@ -42,7 +49,8 @@ namespace MetroidMod.Projectiles
             }
             return base.OnTileCollide(projectile, oldVelocity);
         }*/
-
+		bool init = false;
+		float projSpeed = 1f;
         public override bool PreAI(Projectile projectile)
         {
             if (!projectile.tileCollide && projectile.aiStyle != 26 && !projectile.minion && projectile.damage > 0 && projectile.friendly)
@@ -52,13 +60,63 @@ namespace MetroidMod.Projectiles
                 if (Main.tile[x, y] != null && Main.tile[x, y].active())
                 {
                     if (Main.tile[x, y].type == (ushort)mod.TileType("BlueHatch"))
+                    {
                         TileLoader.HitWire(x, y, mod.TileType("BlueHatch"));
+                    }
                     if (Main.tile[x, y].type == (ushort)mod.TileType("BlueHatchVertical"))
+                    {
                         TileLoader.HitWire(x, y, mod.TileType("BlueHatchVertical"));
+                    }
                 }
             }
+			if(!init)
+			{
+				projSpeed = projectile.velocity.Length();
+				init = true;
+			}
             return base.PreAI(projectile);
         }
+		int counter = 0;
+		public override void AI(Projectile projectile)
+		{
+			float accuracy = 3f;
+			float distance = 250f;
+			if(projectile.active && projectile.penetrate > 0 && projectile.damage > 0 && projectile.friendly && projectile.velocity != Vector2.Zero && projectile.aiStyle != 3 && projectile.aiStyle != 15 && !Main.projPet[projectile.type])
+			{
+				for(int i = 0; i < Main.npc.Length; i++)
+				{
+					NPC npc = Main.npc[i];
+					if(npc != null && npc.active && npc.type == mod.NPCType("OmegaPirateAbsorbField") && npc.ai[3] != 1)
+					{
+						if(Vector2.Distance(npc.Center,projectile.Center) < distance*npc.ai[1])
+						{
+							counter++;
+						}
+						
+						if(counter > 5)
+						{
+							Vector2 vec = npc.Center - projectile.Center;
+							float num2 = vec.Length();
+							num2 = projSpeed / num2;
+							vec *= num2;
+							projectile.velocity.X = (projectile.velocity.X * accuracy + vec.X) / (accuracy + 1f);
+							projectile.velocity.Y = (projectile.velocity.Y * accuracy + vec.Y) / (accuracy + 1f);
+							
+							if(Vector2.Distance(npc.Center,projectile.Center+projectile.velocity) < 55f)
+							{
+								projectile.penetrate = 0;
+								npc.ai[1] = 1.5f;
+								npc.ai[2] += (int)((float)projectile.damage / 10f);
+							}
+						}
+						else
+						{
+							projSpeed = projectile.velocity.Length();
+						}
+					}
+				}
+			}
+		}
         public override void Kill(Projectile projectile, int timeLeft)
         {
             int tilex = (int)(projectile.position.X / 16) - 1;
