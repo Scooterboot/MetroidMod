9a1690bea9289b79a863237736b9098c22161178
Scooterboot <scooterboot2012@gmail.com>
2018-11-03 15:05:22 -0500

    Fixed error when unequipping Morph Ball while it's active
    If you unequip Morph Ball while the mount is active, you'll still be in the mount, but lose the addons (re-equipping it will re-add the addon effects).

--- Mounts/MorphBallMount.cs
+++ Mounts/MorphBallMount.cs
@@ -22,8 +22,8 @@ namespace MetroidMod.Mounts
 			mountData.spawnDust = 299;//63;
 			mountData.spawnDustNoGravity = true;
 			mountData.buff = mod.BuffType("MorphBallMountBuff");
-            mountData.heightBoost = -(42 - MPlayer.morphSize);
-            mountData.runSpeed = 3f;
+			mountData.heightBoost = -(42 - MPlayer.morphSize);
+			mountData.runSpeed = 3f;
 			mountData.acceleration = 0.08f;
 			mountData.dashSpeed = 3f;
 			
@@ -41,12 +41,20 @@ namespace MetroidMod.Mounts
 			mountData.playerYOffsets = array;
 			mountData.playerHeadOffset = mountData.heightBoost;
 		}
-        
+		
+		MorphBall mBall;
 		public override void UpdateEffects(Player player)
 		{
 			MPlayer mp = player.GetModPlayer<MPlayer>(mod);
 			
-			MorphBall mBall = (MorphBall)player.miscEquips[3].modItem;
+			if(player.miscEquips[3].type == mod.ItemType("MorphBall"))
+			{
+				mBall = (MorphBall)player.miscEquips[3].modItem;
+			}
+			else
+			{
+				mBall = null;
+			}
 
 			int pb = mod.ItemType("PowerBombAddon");
 			int sb = mod.ItemType("SpiderBallAddon");
@@ -54,31 +62,42 @@ namespace MetroidMod.Mounts
 
 			mp.morphBall = true;
 			mp.MorphBallBasic(player);
-			if (!mBall.ballMods[0].IsAir)
+			if(mBall != null)
 			{
-				MGlobalItem drillMItem = mBall.ballMods[0].GetGlobalItem<MGlobalItem>(mod);
-				mp.Drill(player,drillMItem.drillPower);
-			}
-			if (!mBall.ballMods[1].IsAir)
-			{
-				MGlobalItem bombMItem = mBall.ballMods[1].GetGlobalItem<MGlobalItem>(mod);
-				mp.bombDamage = (int)(player.rangedDamage * bombMItem.bombDamage);
-				mp.Bomb(player);
-			}
-			if (mBall.ballMods[2].type == pb)
-				mp.PowerBomb(player);
+				if (!mBall.ballMods[0].IsAir)
+				{
+					MGlobalItem drillMItem = mBall.ballMods[0].GetGlobalItem<MGlobalItem>(mod);
+					mp.Drill(player,drillMItem.drillPower);
+				}
+				if (!mBall.ballMods[1].IsAir)
+				{
+					MGlobalItem bombMItem = mBall.ballMods[1].GetGlobalItem<MGlobalItem>(mod);
+					mp.bombDamage = (int)(player.rangedDamage * bombMItem.bombDamage);
+					mp.Bomb(player);
+				}
+				if (mBall.ballMods[2].type == pb)
+				{
+					mp.PowerBomb(player);
+				}
 
-			if (mBall.ballMods[3].type == sb)
-				mp.SpiderBall(player);
-			else
-				mp.spiderball = false;
+				if (mBall.ballMods[3].type == sb)
+				{
+					mp.SpiderBall(player);
+				}
+				else
+				{
+					mp.spiderball = false;
+				}
 
-			if (mBall.ballMods[4].type == bb)
-				mp.BoostBall(player);
-			else
-			{
-				mp.boostCharge = 0;
-				mp.boostEffect = 0;
+				if (mBall.ballMods[4].type == bb)
+				{
+					mp.BoostBall(player);
+				}
+				else
+				{
+					mp.boostCharge = 0;
+					mp.boostEffect = 0;
+				}
 			}
 		}
 	}
