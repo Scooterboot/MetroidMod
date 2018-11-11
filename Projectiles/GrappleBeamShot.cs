60be86da052788ac4f8ae3999d532788880869ed
Scooterboot <scooterboot2012@gmail.com>
2018-11-10 19:43:56 -0600

    Revamped Grapple Beam visuals

--- Projectiles/GrappleBeamShot.cs
+++ Projectiles/GrappleBeamShot.cs
@@ -12,16 +12,20 @@ namespace MetroidMod.Projectiles
 {
 	public class GrappleBeamShot : ModProjectile
 	{
-        internal int[] GrappableNPCs = new int[]
-            {
-                ModLoader.GetMod("MetroidMod").NPCType("Ripper"),
-                ModLoader.GetMod("MetroidMod").NPCType("Powamp")
-            };
+		internal int[] GrappableNPCs = new int[]
+		{
+			ModLoader.GetMod("MetroidMod").NPCType("Ripper"),
+			ModLoader.GetMod("MetroidMod").NPCType("Powamp")
+		};
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Grapple Beam");
-
+			Main.projFrames[projectile.type] = 2;
 		}
+		int chainFrame = 0;
+		int chainFrame2 = 0;
+		float time = 0f;
+		float increment = 0f;
 		public override void SetDefaults()
 		{
 			projectile.CloneDefaults(ProjectileID.GemHookAmethyst);
@@ -92,6 +96,7 @@ namespace MetroidMod.Projectiles
 		public override bool PreAI()
 		{
 			owner = Main.player[projectile.owner];
+			increment = ((float)Math.PI*2)/60f;
 			return false;
 		}
 		public override void PostAI()
@@ -105,79 +110,105 @@ namespace MetroidMod.Projectiles
 			Projectile P = projectile;
 			MPlayer mp = owner.GetModPlayer<MPlayer>(mod);
 			if (isHooked)
-            {
-                P.ai[0] = 2f;
-                P.velocity = default(Vector2);
-                P.timeLeft = 2;
+			{
+				P.ai[0] = 2f;
+				P.velocity = default(Vector2);
+				P.timeLeft = 2;
 
-                if (P.ai[1] <= 0)
-                {
-                    int num124 = (int)(P.position.X / 16f) - 1;
-                    int num125 = (int)((P.position.X + (float)P.width) / 16f) + 2;
-                    int num126 = (int)(P.position.Y / 16f) - 1;
-                    int num127 = (int)((P.position.Y + (float)P.height) / 16f) + 2;
-                    if (num124 < 0)
-                        num124 = 0;
-                    if (num125 > Main.maxTilesX)
-                        num125 = Main.maxTilesX;
-                    if (num126 < 0)
-                        num126 = 0;
-                    if (num127 > Main.maxTilesY)
-                        num127 = Main.maxTilesY;
+				if (P.ai[1] <= 0)
+				{
+					int num124 = (int)(P.position.X / 16f) - 1;
+					int num125 = (int)((P.position.X + (float)P.width) / 16f) + 2;
+					int num126 = (int)(P.position.Y / 16f) - 1;
+					int num127 = (int)((P.position.Y + (float)P.height) / 16f) + 2;
+					if (num124 < 0)
+					{
+						num124 = 0;
+					}
+					if (num125 > Main.maxTilesX)
+					{
+						num125 = Main.maxTilesX;
+					}
+					if (num126 < 0)
+					{
+						num126 = 0;
+					}
+					if (num127 > Main.maxTilesY)
+					{
+						num127 = Main.maxTilesY;
+					}
 
-                    bool flag3 = true;
-                    for (int x = num124; x < num125; x++)
-                    {
-                        for (int y = num126; y < num127; y++)
-                        {
-                            if (Main.tile[x, y] == null)
-                                Main.tile[x, y] = new Tile();
+					bool flag3 = true;
+					for (int x = num124; x < num125; x++)
+					{
+						for (int y = num126; y < num127; y++)
+						{
+							if (Main.tile[x, y] == null)
+							{
+								Main.tile[x, y] = new Tile();
+							}
 
-                            Tile tile = Main.tile[x, y];
+							Tile tile = Main.tile[x, y];
 
-                            Vector2 vector9;
-                            vector9.X = x * 16;
-                            vector9.Y = y * 16;
-                            if (P.position.X + (float)(P.width / 2) > vector9.X && P.position.X + (float)(P.width / 2) < vector9.X + 16f && P.position.Y + (float)(P.height / 2) > vector9.Y && P.position.Y + (float)(P.height / 2) < vector9.Y + 16f && tile.nactive() && (Main.tileSolid[tile.type] || tile.type == 314))
-                                flag3 = false;
-                        }
-                    }
-                    if (flag3)
-                        isHooked = false;
-                    else //if (owner.grapCount < 10)
-                        mp.grapplingBeam = P.whoAmI;
-                        //owner.grapCount++;
-                }
-                else // Hooked onto NPC
-                {
-                    if (Main.npc[(int)projectile.ai[1]].active)
-                    {
-                        NPC target = Main.npc[(int)projectile.ai[1]];
-                        mp.grapplingBeam = P.whoAmI;
-                        projectile.position = target.position + new Vector2(0, target.height / 2);
-                        projectile.velocity = Vector2.Zero;
-                    }
-                    else
-                        isHooked = false;
-                }
+							Vector2 vector9;
+							vector9.X = x * 16;
+							vector9.Y = y * 16;
+							if (P.position.X + (float)(P.width / 2) > vector9.X && P.position.X + (float)(P.width / 2) < vector9.X + 16f && P.position.Y + (float)(P.height / 2) > vector9.Y && P.position.Y + (float)(P.height / 2) < vector9.Y + 16f && tile.nactive() && (Main.tileSolid[tile.type] || tile.type == 314))
+							{
+								flag3 = false;
+							}
+						}
+					}
+					if (flag3)
+					{
+						isHooked = false;
+					}
+					else //if (owner.grapCount < 10)
+					{
+						mp.grapplingBeam = P.whoAmI;
+						//owner.grapCount++;
+					}
+				}
+				else // Hooked onto NPC
+				{
+					if (Main.npc[(int)projectile.ai[1]].active)
+					{
+						NPC target = Main.npc[(int)projectile.ai[1]];
+						mp.grapplingBeam = P.whoAmI;
+						projectile.position = target.position + new Vector2(0, target.height / 2);
+						projectile.velocity = Vector2.Zero;
+					}
+					else
+					{
+						isHooked = false;
+					}
+				}
 			}
 			else
 			{
 				P.ai[0] = 0f;
 
-                // Tile check.
+				// Tile check.
 				int num111 = (int)(P.position.X / 16f) - 1;
 				int num112 = (int)((P.position.X + (float)P.width) / 16f) + 2;
 				int num113 = (int)(P.position.Y / 16f) - 1;
 				int num114 = (int)((P.position.Y + (float)P.height) / 16f) + 2;
 				if (num111 < 0)
+				{
 					num111 = 0;
+				}
 				if (num112 > Main.maxTilesX)
+				{
 					num112 = Main.maxTilesX;
+				}
 				if (num113 < 0)
+				{
 					num113 = 0;
+				}
 				if (num114 > Main.maxTilesY)
+				{
 					num114 = Main.maxTilesY;
+				}
 
 				for (int x = num111; x < num112; x++)
 				{
@@ -185,11 +216,13 @@ namespace MetroidMod.Projectiles
 					while (y < num114)
 					{
 						if (Main.tile[x, y] == null)
+						{
 							Main.tile[x, y] = new Tile();
+						}
 
-                        Tile tile = Main.tile[x, y];
+						Tile tile = Main.tile[x, y];
 
-                        Vector2 vector8;
+						Vector2 vector8;
 						vector8.X = x * 16;
 						vector8.Y = y * 16;
 						if (P.position.X + P.width > vector8.X && P.position.X < vector8.X + 16f && P.position.Y + P.height > vector8.Y && P.position.Y < vector8.Y + 16f && tile.nactive() && (Main.tileSolid[tile.type] || tile.type == 314))
@@ -199,10 +232,10 @@ namespace MetroidMod.Projectiles
 							//if (owner.grapCount < 10)
 							//{
 								mp.grapplingBeam = P.whoAmI;
-                            //owner.grapCount++;
-                            //}
+								//owner.grapCount++;
+							//}
 
-                            P.velocity *= 0;
+							P.velocity *= 0;
 							isHooked = true;
 							P.position.X = (float)(x * 16 + 8 - P.width / 2);
 							P.position.Y = (float)(y * 16 + 8 - P.height / 2);
@@ -230,24 +263,26 @@ namespace MetroidMod.Projectiles
 					}
 				}
 
-                // NPC check.
-                for(int i = 0; i < 200; ++i)
-                {
-                    if(projectile.getRect().Intersects(Main.npc[i].getRect()))
-                    {
-                        NPC target = Main.npc[i];
+				// NPC check.
+				for(int i = 0; i < 200; ++i)
+				{
+					NPC target = Main.npc[i];
+					if(projectile.getRect().Intersects(target.getRect()) && target.active)
+					{
+						if(GrappableNPCs.Contains(target.type))
+						{
+							projectile.ai[1] = i;
 
-                        if(GrappableNPCs.Contains(target.type))
-                        {
-                            projectile.ai[1] = i;
-
-                            projectile.velocity *= 0;
-                            isHooked = true;
-                            projectile.netUpdate = true;
-                            break;
-                        }
-                    }
-                }
+							projectile.velocity *= 0;
+							isHooked = true;
+							projectile.netUpdate = true;
+							Main.PlaySound(SoundLoader.customSoundType, (int)owner.Center.X, (int)owner.Center.Y, mod.GetSoundSlot(SoundType.Custom, "Sounds/GrappleLatch"));
+							mp.maxDist = Vector2.Distance(owner.Center, P.Center);
+							mp.grapplingBeam = P.whoAmI;
+							break;
+						}
+					}
+				}
 			}
 
 			if (Main.myPlayer == P.owner)
@@ -258,19 +293,39 @@ namespace MetroidMod.Projectiles
 				int maxAllowedGrapples = 1;
 				for (int i = 0; i < 1000; i++)
 				{
-                    Projectile targetProj = Main.projectile[i];
+					Projectile targetProj = Main.projectile[i];
 					if (targetProj.active && targetProj.owner == P.owner && (targetProj.type == P.type || targetProj.aiStyle == 7))
 					{
 						if (targetProj.timeLeft < oldestProjectileTimeLeft)
 						{
-                            oldestProjectile = i;
-                            oldestProjectileTimeLeft = targetProj.timeLeft;
+							oldestProjectile = i;
+							oldestProjectileTimeLeft = targetProj.timeLeft;
 						}
-                        amountOfGrapples++;
+						amountOfGrapples++;
 					}
 				}
 				if (amountOfGrapples > maxAllowedGrapples)
+				{
 					Main.projectile[oldestProjectile].Kill();
+				}
+			}
+			
+			P.frameCounter++;
+			if(P.frameCounter > 4)
+			{
+				P.frame++;
+				P.frameCounter = 0;
+			}
+			if(P.frame >= Main.projFrames[P.type])
+			{
+				P.frame = 0;
+			}
+			chainFrame = Main.rand.Next(4);
+			chainFrame2 = Main.rand.Next(4);
+			time += increment * 2f;
+			if(time >= (float)Math.PI*2)
+			{
+				time -= (float)Math.PI*2;
 			}
 		}
 		public override bool PreKill(int timeLeft)
@@ -281,19 +336,83 @@ namespace MetroidMod.Projectiles
 
 		public override bool PreDraw(SpriteBatch s, Color lightColor)
 		{
-			this.DrawChain(owner.Center, projectile.Center, mod.GetTexture("Gore/GrappleBeamChain"), s);
-			return true;
+			Projectile P = projectile;
+			MPlayer mp = owner.GetModPlayer<MPlayer>(mod);
+			
+			Texture2D tex = mod.GetTexture("Projectiles/GrappleBeamChain2");
+			float dist = Math.Max(Vector2.Distance(owner.Center, projectile.Center),1);
+			float maxDist = 400;
+			if(isHooked)
+			{
+				maxDist = Math.Max(mp.maxDist,1);
+			}
+			
+			int numH = tex.Height / 4;
+			
+			Vector2 chain = projectile.Center - owner.Center;
+			int linklength = Math.Max(numH-1,1);
+			int numlinks = (int)Math.Ceiling(dist/linklength);
+			Vector2[] pos = new Vector2[numlinks];
+			
+			float amplitude = 3 + 6f * (1f - dist/maxDist);
+			
+			for(int j = 0; j < 3; j++)
+			{
+				if(j == 0 || j == 2)
+				{
+					for(int i = 0; i < numlinks; i++)
+					{
+						pos[i] = owner.Center + chain/numlinks * i;
+						
+						int k = 1;
+						if(j > 0)
+						{
+							k = -1;
+						}
+						
+						float t = 0f;
+						t += increment * (i * maxDist/dist) * 2f;
+						if(t >= (float)Math.PI*2)
+						{
+							t -= (float)Math.PI*2;
+						}
+						
+						float shift = amplitude * (float)Math.Sin(t + time) * k;
+						
+						float rot = (float)Math.Atan2(chain.Y, chain.X) + (float)Math.PI/2;
+						pos[i].X += (float)Math.Cos(rot)*shift;
+						pos[i].Y += (float)Math.Sin(rot)*shift;
+						
+						Color color = Color.White;
+						s.Draw(tex,pos[i] - Main.screenPosition,new Rectangle?(new Rectangle(0,numH*chainFrame2,tex.Width,numH)),color,rot,new Vector2((float)tex.Width/2,(float)numH/2),projectile.scale,SpriteEffects.None,0f);
+					}
+				}
+				else
+				{
+					this.DrawChain(owner.Center, projectile.Center, mod.GetTexture("Projectiles/GrappleBeamChain"), s, chainFrame, 4);
+				}
+			}
+			tex = Main.projectileTexture[P.type];
+			int num = tex.Height / Main.projFrames[projectile.type];
+			Main.spriteBatch.Draw(tex, projectile.Center - Main.screenPosition, new Rectangle?(new Rectangle(0, num*P.frame, tex.Width, num)), projectile.GetAlpha(Color.White), 0f, new Vector2((float)tex.Width/2, (float)num/2), projectile.scale, SpriteEffects.None, 0f);
+			return false;
 		}
 		public override bool PreDrawExtras(SpriteBatch spriteBatch)
 		{
 			return false;
 		}
-		public void DrawChain(Vector2 start, Vector2 end, Texture2D name, SpriteBatch spriteBatch)
+		public void DrawChain(Vector2 start, Vector2 end, Texture2D name, SpriteBatch spriteBatch, int frame = 0, int frameCount = 0)
 		{
+			int numH = name.Height;
+			if(frameCount > 0)
+			{
+				numH = name.Height/frameCount;
+			}
+			
 			start -= Main.screenPosition;
 			end -= Main.screenPosition;
 
-			int linklength = name.Height;
+			int linklength = numH-1;
 			Vector2 chain = end - start;
 
 			float length = (float)chain.Length();
@@ -307,9 +426,8 @@ namespace MetroidMod.Projectiles
 				Vector2 LR = links[i]+Main.screenPosition;
 
 				Color color = Lighting.GetColor((int)((links[i].X+Main.screenPosition.X)/16), (int)((links[i].Y+Main.screenPosition.Y)/16));
-				spriteBatch.Draw(name,
-				new Rectangle((int)links[i].X, (int)links[i].Y, name.Width, linklength), null,
-				color, rotation+1.57f, new Vector2(name.Width/2f, linklength), SpriteEffects.None, 1f);
+				//spriteBatch.Draw(name, new Rectangle((int)links[i].X, (int)links[i].Y, name.Width, linklength), null, color, rotation+1.57f, new Vector2(name.Width/2f, linklength), SpriteEffects.None, 1f);
+				spriteBatch.Draw(name,links[i],new Rectangle?(new Rectangle(0,numH*frame,name.Width,numH)),color,rotation+1.57f,new Vector2(name.Width/2f,numH/2f),projectile.scale,SpriteEffects.None,0f);
 
 				Lighting.AddLight(LR, 229f/255f, 249f/255f, 255f/255f);
 			}
