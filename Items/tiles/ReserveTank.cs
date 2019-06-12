abeeac7ddfb6e4d6e13f39d9f89b466de561a0b1
Joost8910 <bluedog@fenria.com>
2019-06-12 18:37:39 -0500

    Add files via upload

--- Items/tiles/ReserveTank.cs
+++ Items/tiles/ReserveTank.cs
@@ -16,7 +16,7 @@ namespace MetroidMod.Items.tiles
 		{
 			item.width = 32;
 			item.height = 32;
-			item.maxStack = 999;
+			item.maxStack = 1;
 			item.useTurn = true;
 			item.autoReuse = true;
 			item.useAnimation = 15;
@@ -25,13 +25,33 @@ namespace MetroidMod.Items.tiles
 			item.consumable = true;
 			item.createTile = mod.TileType("ReserveTank");
 			item.rare = 2;
-			item.value = 1000;
+			item.value = 20000;
             item.accessory = true;
 		}
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             MPlayer mp = player.GetModPlayer<MPlayer>(mod);
             mp.reserveTanks = 1;
+        }
+        public override bool CanEquipAccessory(Player player, int slot)
+        {
+            for (int k = 3; k < 8 + player.extraAccessorySlots; k++)
+            {
+                if (player.armor[k].type == mod.ItemType("ReserveTank2") || player.armor[k].type == mod.ItemType("ReserveTank3") || player.armor[k].type == mod.ItemType("ReserveTank4") || player.armor[k].type == mod.ItemType("ReserveTank5"))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "EnergyTank", 1);
+            recipe.AddRecipeGroup("IronBar", 5);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
         }
     }
 }
