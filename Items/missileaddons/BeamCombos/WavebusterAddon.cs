15b6ff625ba49e10666e4afe37a8055c260955ff
Scooterboot <scooterboot2012@gmail.com>
2019-07-03 20:28:42 -0500

    Uploaded Beam Combo item files
    Sprites are placeholder, as are their recipes.

---
+++ Items/missileaddons/BeamCombos/WavebusterAddon.cs
@@ -0,0 +1,44 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MetroidMod.Items.missileaddons.BeamCombos
+{
+	public class WavebusterAddon : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Wavebuster");
+			Tooltip.SetDefault(string.Format("[c/9696FF:Missile Launcher Addon]\n") +
+			"Slot Type: Charge\n" +
+			"Hold Click to charge\n" + 
+			"~Fires a constant stream of electricity at full charge\n" + 
+			"~Drains 5 missiles per second during use");
+		}
+		public override void SetDefaults()
+		{
+			item.width = 10;
+			item.height = 14;
+			item.maxStack = 1;
+			item.value = 2500;
+			item.rare = 4;
+			/*item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = 1;
+			item.consumable = true;
+			item.createTile = mod.TileType("WavebusterTile");*/
+			MGlobalItem mItem = item.GetGlobalItem<MGlobalItem>(mod);
+			mItem.missileSlotType = 0;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock, 1);
+			//recipe.AddTile(TileID.MythrilAnvil);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
