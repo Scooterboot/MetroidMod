88369ef18c2f5a01c3b5ac19173ef7f19c87b97f
Scooterboot <scooterboot2012@gmail.com>
2019-07-23 14:49:41 -0500

    Update FlamethrowerLead.cs

--- Projectiles/missilecombo/FlamethrowerLead.cs
+++ Projectiles/missilecombo/FlamethrowerLead.cs
@@ -26,7 +26,7 @@ namespace MetroidMod.Projectiles.missilecombo
 			projectile.friendly = false;
 		}
 		Projectile Lead;
-		int useTime = 0;
+		//int useTime = 0;
 		SoundEffectInstance soundInstance;
 		bool soundPlayed = false;
 		int soundDelay = 0;
@@ -50,10 +50,33 @@ namespace MetroidMod.Projectiles.missilecombo
 					diff.Normalize();
 					P.velocity = diff * 8f;
 					P.netUpdate = true;
+					
+					if(soundDelay <= 0)
+					{
+						if(!soundPlayed)
+						{
+							soundInstance = Main.PlaySound(SoundLoader.customSoundType, (int)O.position.X, (int)O.position.Y, mod.GetSoundSlot(SoundType.Custom, "Sounds/FlamethrowerStart"));
+							soundPlayed = true;
+							soundDelay = 132;
+						}
+						else
+						{
+							if(soundInstance != null)
+							{
+								soundInstance.Stop(true);
+							}
+							soundInstance = Main.PlaySound(SoundLoader.customSoundType, (int)O.position.X, (int)O.position.Y, mod.GetSoundSlot(SoundType.Custom, "Sounds/FlamethrowerLoop"));
+							soundDelay = 117;
+						}
+					}
+					else
+					{
+						soundDelay--;
+					}
 				}
 				P.Center = oPos;
 				P.timeLeft = 2;
-				if(useTime <= 0)
+				/*if(useTime <= 0)
 				{
 					int proj = Projectile.NewProjectile(oPos.X, oPos.Y, P.velocity.X, P.velocity.Y, mod.ProjectileType("FlamethrowerShot"), P.damage, P.knockBack, P.owner);
 					useTime = 2;
@@ -61,30 +84,7 @@ namespace MetroidMod.Projectiles.missilecombo
 				else
 				{
 					useTime--;
-				}
-				
-				if(soundDelay <= 0)
-				{
-					if(!soundPlayed)
-					{
-						soundInstance = Main.PlaySound(SoundLoader.customSoundType, (int)O.position.X, (int)O.position.Y, mod.GetSoundSlot(SoundType.Custom, "Sounds/FlamethrowerStart"));
-						soundPlayed = true;
-						soundDelay = 132;
-					}
-					else
-					{
-						if(soundInstance != null)
-						{
-							soundInstance.Stop(true);
-						}
-						soundInstance = Main.PlaySound(SoundLoader.customSoundType, (int)O.position.X, (int)O.position.Y, mod.GetSoundSlot(SoundType.Custom, "Sounds/FlamethrowerLoop"));
-						soundDelay = 117;
-					}
-				}
-				else
-				{
-					soundDelay--;
-				}
+				}*/
 			}
 		}
 		public override void Kill(int timeLeft)
