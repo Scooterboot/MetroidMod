800202bb8725ce4c40e3c91dd573862bdcb43bc4
Scooterboot <scooterboot2012@gmail.com>
2019-07-28 13:48:16 -0500

    Update SolarLaserShot.cs

--- Projectiles/missilecombo/SolarLaserShot.cs
+++ Projectiles/missilecombo/SolarLaserShot.cs
@@ -85,7 +85,7 @@ namespace MetroidMod.Projectiles.missilecombo
 				for (P.ai[1] = 0f; P.ai[1] <= maxRange; P.ai[1] += 4f)
 				{
 					Vector2 end = oPos + P.velocity * P.ai[1];
-					if (!Collision.CanHit(oPos, 1, 1, end, 1, 1))
+					if(CollideMethods.CheckCollide(end, 0, 0))
 					{
 						P.ai[1] -= 4f;
 						if(num <= 0)
