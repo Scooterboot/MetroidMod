5bfd7501959ceb59fe89e71d059b58050d7cb4f8
Scooterboot <scooterboot2012@gmail.com>
2019-08-19 17:32:54 -0500

    Uploaded Nebula Combo
    Also edited the Solar Combo's placeholder sprite to fall inline with the others. (Probably not necessary since, well, it's a placeholder.)

---
+++ Items/missileaddons/BeamCombos/NebulaComboAddon.cs
@@ -0,0 +1,46 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MetroidMod.Items.missileaddons.BeamCombos
+{
+	public class NebulaComboAddon : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Nebula Singularity");
+			Tooltip.SetDefault(string.Format("[c/9696FF:Missile Launcher Addon]\n") +
+			"Slot Type: Charge\n" +
+			"Hold Click to charge\n" + 
+			"~Fires a continuous 'something something' at full charge\n" + 
+			"~Drains # missiles per second during use");
+		}
+		public override void SetDefaults()
+		{
+			item.width = 10;
+			item.height = 14;
+			item.maxStack = 1;
+			item.value = 2500;
+			item.rare = 4;
+			/*item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = 1;
+			item.consumable = true;
+			item.createTile = mod.TileType("NebulaComboAddonTile");*/
+			MGlobalItem mItem = item.GetGlobalItem<MGlobalItem>(mod);
+			mItem.missileSlotType = 0;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.FragmentNebula, 15);
+            recipe.AddIngredient(ItemID.LunarBar, 5);
+            recipe.AddIngredient(ItemID.Amethyst, 5);
+			recipe.AddTile(TileID.LunarCraftingStation);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
