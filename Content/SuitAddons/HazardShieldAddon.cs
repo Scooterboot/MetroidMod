d162b02764f7a108a1e9404907f7067d069b76ee
AmiableMarker65 <wild.batman@outlook.com>
2022-05-10 13:01:54 -0400

    Port PED Suit and Hazard Shield

---
+++ Content/SuitAddons/HazardShieldAddon.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using MetroidModPorted.Common.Players;
+using MetroidModPorted.ID;
+
+namespace MetroidModPorted.Content.SuitAddons
+{
+	internal class HazardShieldAddon : ModSuitAddon
+	{
+		public override string ItemTexture => $"{Mod.Name}/Assets/Textures/SuitAddons/HazardShieldSuit/HazardShieldSuitItem";
+
+		public override string TileTexture => $"{Mod.Name}/Assets/Textures/SuitAddons/HazardShieldSuit/HazardShieldSuitTile";
+
+		public override string ArmorTextureHead => $"{Mod.Name}/Assets/Textures/SuitAddons/HazardShieldSuit/HazardShieldSuitHelmet_Head";
+
+		public override string ArmorTextureTorso => $"{Mod.Name}/Assets/Textures/SuitAddons/HazardShieldSuit/HazardShieldSuitBreastplate_Body";
+
+		public override string ArmorTextureArmsGlow => $"{Mod.Name}/Assets/Textures/SuitAddons/HazardShieldSuit/HazardShieldSuitBreastplate_Arms_Glow";
+
+		public override string ArmorTextureLegs => $"{Mod.Name}/Assets/Textures/SuitAddons/HazardShieldSuit/HazardShieldSuitGreaves_Legs";
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Hazard Shield");
+			Tooltip.SetDefault("You shouldn't have this."/*"+10 defense\n" +
+				"+15 overheat capacity\n" +
+				"5% decreased overheat use\n" +
+				"5% decreased Missile Charge Combo cost\n" +
+				"5% increased hunter damage\n" +
+				"5% increased hunter critical strike chance\n" +
+				"10% increased movement speed\n" +
+				"Debuffs tick down twice as fast"*/);
+			AddonSlot = SuitAddonSlotID.Suit_Augment;
+			ItemNameLiteral = false;
+		}
+		public override void SetItemDefaults(Item item)
+		{
+			item.value = Terraria.Item.buyPrice(0, 11, 70, 0);
+			item.rare = ItemRarityID.Lime;
+		}
+		public override void OnUpdateArmorSet(Player player)
+		{
+			player.statDefense += 10;
+			player.moveSpeed += 0.10f;
+			MPlayer mp = player.GetModPlayer<MPlayer>();
+			HunterDamagePlayer.ModPlayer(player).HunterDamageMult += 0.05f;
+			HunterDamagePlayer.ModPlayer(player).HunterCrit += 5;
+			mp.phazonRegen = 4;
+			mp.maxOverheat += 15;
+			mp.overheatCost -= 0.05f;
+			mp.missileCost -= 0.05f;
+			mp.hazardShield = 1;
+		}
+		public override void OnUpdateVanitySet(Player player)
+		{
+			player.GetModPlayer<MPlayer>().visorGlowColor = new Color(0, 228, 255);
+		}
+	}
+}
---
+++ Content/SuitAddons/PEDSuitAddon.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using MetroidModPorted.Common.Players;
+using MetroidModPorted.ID;
+
+namespace MetroidModPorted.Content.SuitAddons
+{
+	public class PEDSuitAddon : ModSuitAddon
+	{
+		public override string ItemTexture => $"{Mod.Name}/Assets/Textures/SuitAddons/PEDSuit/PEDSuitItem";
+
+		public override string TileTexture => $"{Mod.Name}/Assets/Textures/SuitAddons/PEDSuit/PEDSuitTile";
+
+		public override string ArmorTextureHead => $"{Mod.Name}/Assets/Textures/SuitAddons/PEDSuit/PEDSuitHelmet_Head";
+
+		public override string ArmorTextureTorso => $"{Mod.Name}/Assets/Textures/SuitAddons/PEDSuit/PEDSuitBreastplate_Body";
+
+		public override string ArmorTextureArmsGlow => $"{Mod.Name}/Assets/Textures/SuitAddons/PEDSuit/PEDSuitBreastplate_Arms_Glow";
+
+		public override string ArmorTextureLegs => $"{Mod.Name}/Assets/Textures/SuitAddons/PEDSuit/PEDSuitGreaves_Legs";
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("PED Suit");
+			Tooltip.SetDefault("You shouldn't have this."/*"+9 defense\n" +
+				"+15 overheat capacity\n" +
+				"5% decreased overheat use\n" +
+				"5% decreased Missile Charge Combo cost\n" +
+				"5% increased hunter damage\n" +
+				"3% increased hunter critical strike chance\n" +
+				"Press the Hypermode key to activate Hypermode (take 100 damage to gain +50% damage for 20 seconds, 120 s cooldown)\n" +
+				"Slightly increased health regen when standing on Phazon"*/);
+			AddonSlot = SuitAddonSlotID.Suit_Utility;
+			ItemNameLiteral = false;
+		}
+		public override void SetItemDefaults(Item item)
+		{
+			item.value = Terraria.Item.buyPrice(0, 7, 80, 0);
+			item.rare = ItemRarityID.Pink;
+		}
+		public override void OnUpdateArmorSet(Player player)
+		{
+			player.statDefense += 9;
+			MPlayer mp = player.GetModPlayer<MPlayer>();
+			HunterDamagePlayer.ModPlayer(player).HunterDamageMult += 0.05f;
+			HunterDamagePlayer.ModPlayer(player).HunterCrit += 3;
+			mp.phazonImmune = true;
+			mp.phazonRegen = 2;
+			mp.maxOverheat += 15;
+			mp.overheatCost -= 0.05f;
+			mp.missileCost -= 0.05f;
+			// code to activate Hypermode goes here; might need to add a Hypermode hook to MPlayer like Sense Move
+		}
+		public override void OnUpdateVanitySet(Player player)
+		{
+			player.GetModPlayer<MPlayer>().visorGlowColor = new Color(0, 228, 255);
+		}
+	}
+}
