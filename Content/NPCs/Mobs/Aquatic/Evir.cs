703a84a00a6e92a2785d22f92300e77d1d5d3cef
AmiableMarker65 <wild.batman@outlook.com>
2022-05-17 08:21:52 -0400

    Fixed namespace+folder issues

---
+++ Content/NPCs/Mobs/Aquatic/Evir.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MetroidModPorted.Content.NPCs.Mobs.Aquatic
+{
+	public class Evir : MNPC
+	{
+		/*
+		 * NPC.ai[0] = state manager.
+		 * NPC.ai[1] = timer.
+		 * NPC.ai[2] = projectile index.
+		 */
+		internal readonly float shootSpeed = 10;
+		internal readonly Vector2 projectilePosition = new Vector2(16, 35);
+		public override void SetStaticDefaults()
+		{
+			Main.npcFrameCount[Type] = 4;
+		}
+
+		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+		{
+			bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
+
+				new FlavorTextBestiaryInfoElement("This is a native creature from the depths of Zebes. After the crash of the pirates, they managed to adapt to the oceans of this world. They have a hard outer shell, with a weak chest. Hit their belly to hurt them.")
+			});
+		}
+		public override void SetDefaults()
+		{
+			NPC.width = 30; NPC.height = 34;
+
+			/* Temporary NPC values */
+			NPC.scale = 2;
+			NPC.damage = 15;
+			NPC.defense = 5;
+			NPC.lifeMax = 150;
+			NPC.aiStyle = -1;
+			NPC.knockBackResist = 0;
+
+			NPC.noGravity = true;
+			NPC.HitSound = SoundID.NPCHit1;
+			NPC.DeathSound = SoundID.NPCDeath1;
+		}
+
+		public override bool PreAI()
+		{
+			NPC.TargetClosest();
+
+			if(NPC.ai[0] == 0)
+			{
+				if (NPC.ai[1]++ >= 120)
+				{
+					NPC.ai[0] = 1;
+					NPC.ai[1] = 0;
+					NPC.ai[2] = Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, Vector2.Zero, ModContent.ProjectileType<Projectiles.Mobs.EvirProjectile>(), NPC.damage, .3F, Main.myPlayer, NPC.whoAmI);
+					Main.projectile[(int)NPC.ai[2]].scale = NPC.scale;
+				}
+			}
+			else
+			{
+				Projectile projectile = Main.projectile[(int)NPC.ai[2]];
+				projectile.position = NPC.position + (projectilePosition * NPC.scale) - new Vector2(NPC.direction == -1 ? projectile.width+2 : -2, projectile.height / 2);
+
+				if(NPC.ai[1]++ >= 60)
+				{
+					Vector2 shootDirection = Vector2.Normalize(Main.player[NPC.target].Center - projectile.Center) * shootSpeed;
+					projectile.velocity = shootDirection;
+
+					NPC.ai[0] = 0;
+					NPC.ai[1] = 0;
+					NPC.ai[2] = -1;
+				}
+			}
+
+			return false;
+		}
+
+		public override void FindFrame(int frameHeight)
+		{
+			if (NPC.frameCounter++ >= 12)
+			{
+				NPC.frame.Y = (NPC.frame.Y + frameHeight) % (4 * frameHeight);
+				NPC.frameCounter = 0;
+			}
+
+			NPC.spriteDirection = -NPC.direction;
+		}
+	}
+}
---
+++ Content/NPCs/Mobs/Flying/Waver.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MetroidModPorted.Content.NPCs.Mobs.Flying
+{
+	public class Waver : MNPC
+	{
+		/*
+		 * NPC.ai[0] = Y turnaround timer.
+		 */
+		internal readonly float xSpeed = 4;
+		internal readonly float ySpeed = 3;
+
+		public override void SetStaticDefaults()
+		{
+			Main.npcFrameCount[NPC.type] = 5;
+		}
+		public override void SetDefaults()
+		{
+			NPC.width = 22; NPC.height = 16;
+
+			/* Temporary NPC values */
+			NPC.scale = 2;
+			NPC.damage = 15;
+			NPC.defense = 5;
+			NPC.lifeMax = 150;
+			NPC.aiStyle = -1;
+			NPC.knockBackResist = 0;
+
+			NPC.noGravity = true;
+			NPC.HitSound = SoundID.NPCHit1;
+			NPC.DeathSound = SoundID.NPCDeath1;
+		}
+
+		public override bool PreAI()
+		{
+			if (NPC.direction == 0)
+				NPC.direction = 1;
+
+			if (NPC.collideX)
+			{
+				NPC.direction *= -1;
+				NPC.netUpdate = true;
+			}
+			if(NPC.collideY || NPC.ai[0]++ >= 180)
+			{
+				NPC.ai[0] = 0;
+
+				NPC.directionY *= -1;
+				NPC.netUpdate = true;
+			}
+
+			NPC.velocity.X = NPC.direction * xSpeed;
+			NPC.velocity.Y = NPC.directionY * ySpeed;
+
+			return (false);
+		}
+
+		public override void FindFrame(int frameHeight)
+		{
+			int frameCount;
+
+			NPC.frameCounter++;
+			frameCount = Main.npcFrameCount[NPC.type];
+			if (NPC.frameCounter >= 6)
+			{
+				if (NPC.directionY == 1)
+					NPC.frame.Y = (NPC.frame.Y - frameHeight < 0) ? (frameHeight * (frameCount - 1)) : (NPC.frame.Y - frameHeight);
+				else
+					NPC.frame.Y = (NPC.frame.Y + frameHeight) % (frameHeight * frameCount);
+
+				NPC.frameCounter = 0;
+			}
+			NPC.spriteDirection = -NPC.direction;
+		}
+	}
+}
