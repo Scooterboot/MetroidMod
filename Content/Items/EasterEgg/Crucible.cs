3fde67e693a372d58d4ced168ee0c1bc9e410072
DarkSamus49 <wild.batman@outlook.com>
2022-06-15 00:26:37 -0400

    Rip And Tear, until it is done. (Crucible revival)

---
+++ Content/Items/EasterEgg/Crucible.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using MetroidModPorted.Content.Projectiles.EasterEgg;
+
+namespace MetroidModPorted.Content.Items.EasterEgg
+{
+	public class Crucible : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Slayer's Crucible");
+			Tooltip.SetDefault("A hellish blade of unknown origin.\n" +
+			"'Rip and Tear until it is done.'");
+
+			SacrificeTotal = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 100;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 52;
+			Item.height = 52;
+			Item.scale = 1.15f;
+			Item.useTime = 20;
+			Item.useAnimation = 20;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 6;
+			Item.value = Item.buyPrice(gold: 1);
+			Item.rare = ItemRarityID.Red;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+			Item.shoot = ModContent.ProjectileType<CrucibleProj>();
+			Item.shootSpeed = 11f;
+		}
+		public override void MeleeEffects(Player player, Rectangle hitbox)
+		{
+			if (Main.rand.NextBool(2))
+			{
+				Dust dust = Dust.NewDustDirect(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, 183);
+				dust.noGravity = true;
+				dust = Dust.NewDustDirect(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, 6);
+				dust.noGravity = true;
+			}
+		}
+		/*public override bool OnlyShootOnSwing
+		{
+			get {
+				return true;
+			}
+		}*/
+		public override bool CanShoot(Player player) => player.ItemAnimationJustStarted;
+		int lastSound = -1;
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+		{
+			if (!player.ItemAnimationJustStarted) { return false; }
+			int n = 1 + Main.rand.Next(4);
+			if (n == lastSound)
+			{
+				n++;
+				if (n > 4)
+				{
+					n = 1;
+				}
+			}
+			SoundEngine.PlaySound(new($"{Mod.Name}/Assets/Sounds/EasterEgg/Crucible_{n}") { Volume = 0.5f });
+			lastSound = n;
+			return true;
+		}
+		public override void PostDrawInWorld(SpriteBatch sb, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
+		{
+			Texture2D tex = ModContent.Request<Texture2D>($"{Mod.Name}/Content/Items/EasterEgg/Crucible_Glow").Value;
+			float num4 = Item.velocity.X * 0.2f;
+			float scale2 = 1f;
+			float num5 = (float)(Item.height - tex.Height);
+			float num6 = (float)(Item.width / 2 - tex.Width / 2);
+			sb.Draw(tex, new Vector2(Item.position.X - Main.screenPosition.X + (float)(tex.Width / 2) + num6, Item.position.Y - Main.screenPosition.Y + (float)(tex.Height / 2) + num5 + 2f), new Rectangle?(new Rectangle(0, 0, tex.Width, tex.Height)), new Color(250, 250, 250, Item.alpha), num4, new Vector2((float)(tex.Width / 2), (float)(tex.Height / 2)), scale2, SpriteEffects.None, 0f);
+		}
+	}
+}
---
+++ Content/Projectiles/EasterEgg/CrucibleProj.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace MetroidModPorted.Content.Projectiles.EasterEgg
+{
+	public class CrucibleProj : MProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Crucible Shot");
+		}
+		public override void SetDefaults()
+		{
+			Projectile.width = 16;
+			Projectile.height = 16;
+			Projectile.scale = 1f;
+			Projectile.alpha = 255;
+			Projectile.aiStyle = -1;
+			Projectile.DamageType = DamageClass.Melee;
+			Projectile.light = 0.5f;
+			Projectile.friendly = true;
+			Projectile.penetrate = 3;
+		}
+
+		public override void AI()
+		{
+			Projectile.rotation = (float)Math.Atan2((double)Projectile.velocity.Y, (double)Projectile.velocity.X) + 0.785f;
+			Color color = Color.Red;
+			Lighting.AddLight(Projectile.Center, color.R / 255f, color.G / 255f, color.B / 255f);
+
+			int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, 183, 0, 0, 100, default(Color), Projectile.scale);
+			Main.dust[dust].noGravity = true;
+
+			if (Projectile.localAI[1] < 15f)
+			{
+				Projectile.localAI[1] += 1f;
+			}
+			else
+			{
+				if (Projectile.localAI[0] == 0f)
+				{
+					Projectile.scale -= 0.02f;
+					Projectile.alpha += 30;
+					if (Projectile.alpha >= 250)
+					{
+						Projectile.alpha = 255;
+						Projectile.localAI[0] = 1f;
+					}
+				}
+				else if (Projectile.localAI[0] == 1f)
+				{
+					Projectile.scale += 0.02f;
+					Projectile.alpha -= 30;
+					if (Projectile.alpha <= 0)
+					{
+						Projectile.alpha = 0;
+						Projectile.localAI[0] = 0f;
+					}
+				}
+			}
+		}
+		public override void Kill(int timeLeft)
+		{
+			Projectile P = Projectile;
+			SoundEngine.PlaySound(SoundID.Item10, P.position);
+			int num535;
+			for (int num802 = 4; num802 < 31; num802 = num535 + 1)
+			{
+				float num803 = P.oldVelocity.X * (30f / (float)num802);
+				float num804 = P.oldVelocity.Y * (30f / (float)num802);
+				int num805 = Dust.NewDust(new Vector2(P.oldPosition.X - num803, P.oldPosition.Y - num804), 8, 8, 183, P.oldVelocity.X, P.oldVelocity.Y, 255, default(Color), 1.8f);
+				Main.dust[num805].noGravity = true;
+				Dust dust = Main.dust[num805];
+				dust.velocity *= 0.5f;
+				num805 = Dust.NewDust(new Vector2(P.oldPosition.X - num803, P.oldPosition.Y - num804), 8, 8, 6, P.oldVelocity.X, P.oldVelocity.Y, 255, default(Color), 1.4f);
+				dust = Main.dust[num805];
+				dust.velocity *= 0.05f;
+				Main.dust[num805].noGravity = true;
+				num535 = num802;
+			}
+		}
+
+		public override Color? GetAlpha(Color lightColor)
+		{
+			Projectile P = Projectile;
+			if (P.localAI[1] >= 15f)
+			{
+				return new Color(255, 255, 255, P.alpha);
+			}
+			if (P.localAI[1] < 5f)
+			{
+				return Color.Transparent;
+			}
+			int expr_1653 = (int)((P.localAI[1] - 5f) / 10f * 255f);
+			return new Color(expr_1653, expr_1653, expr_1653, expr_1653);
+		}
+		public override bool PreDraw(ref Color lightColor)
+		{
+			Projectile P = Projectile;
+			SpriteEffects effects = SpriteEffects.None;
+			if (Projectile.spriteDirection == -1)
+			{
+				effects = SpriteEffects.FlipHorizontally;
+			}
+
+			Color color25 = Lighting.GetColor((int)((double)P.position.X + (double)P.width * 0.5) / 16, (int)(((double)P.position.Y + (double)P.height * 0.5) / 16.0));
+			Main.spriteBatch.Draw(TextureAssets.Projectile[P.type].Value, new Vector2(P.position.X - Main.screenPosition.X + (float)(P.width / 2), P.position.Y - Main.screenPosition.Y + (float)(P.height / 2)), new Microsoft.Xna.Framework.Rectangle?(new Microsoft.Xna.Framework.Rectangle(0, 0, TextureAssets.Projectile[P.type].Width(), TextureAssets.Projectile[P.type].Height())), P.GetAlpha(color25), P.rotation, new Vector2((float)TextureAssets.Projectile[P.type].Width(), 0f), P.scale, effects, 0f);
+			return false;
+		}
+	}
+}
