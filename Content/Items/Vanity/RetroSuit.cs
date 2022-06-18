2a1391c6b291c4b8b02ca29d62ebf361e32082de
DarkSamus49 <wild.batman@outlook.com>
2022-06-18 14:12:38 -0400

    Retro Suit vanities (NEStroid Power and Varia)

---
+++ Content/Items/Vanity/RetroSuit.cs
@@ -0,0 +1,78 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using MetroidMod.Common.Players;
+
+namespace MetroidMod.Content.Items.Vanity
+{
+	[AutoloadEquip(EquipType.Body)]
+	public class RetroSuitBreastplate : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("'Retro' Power Suit Breastplate");
+
+			SacrificeTotal = 1;
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 18;
+			Item.height = 18;
+			Item.rare = ItemRarityID.Green;
+			Item.vanity = true;
+		}
+		public override bool IsArmorSet(Item head, Item body, Item legs)
+		{
+			return head.type == ModContent.ItemType<RetroSuitHelmet>() && body.type == ModContent.ItemType<RetroSuitBreastplate>() && legs.type == ModContent.ItemType<RetroSuitGreaves>();
+		}
+		public override void UpdateVanitySet(Player P)
+		{
+			MPlayer mp = P.GetModPlayer<MPlayer>();
+			mp.isPowerSuit = true;
+			mp.visorGlowColor = new Color(0, 248, 112);
+			if (P.velocity.Y != 0f && ((P.controlRight && P.direction == 1) || (P.controlLeft && P.direction == -1) || mp.SMoveEffect > 0) && mp.shineDirection == 0 && !mp.shineActive && !mp.ballstate)
+			{
+				mp.jet = true;
+			}
+			else if(mp.shineDirection == 0 || mp.shineDirection == 5)
+			{
+				mp.jet = false;
+			}
+		}
+	}
+	[AutoloadEquip(EquipType.Legs)]
+	public class RetroSuitGreaves : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("'Retro' Power Suit Greaves");
+
+			SacrificeTotal = 1;
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 18;
+			Item.height = 18;
+			Item.rare = ItemRarityID.Green;
+			Item.vanity = true;
+		}
+	}
+	[AutoloadEquip(EquipType.Head)]
+	public class RetroSuitHelmet : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("'Retro' Power Suit Helmet");
+
+			SacrificeTotal = 1;
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 18;
+			Item.height = 18;
+			Item.rare = ItemRarityID.Green;
+			Item.vanity = true;
+		}
+	}
+}
---
+++ Content/Items/Vanity/RetroVariaSuit.cs
@@ -0,0 +1,78 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using MetroidMod.Common.Players;
+
+namespace MetroidMod.Content.Items.Vanity
+{
+	[AutoloadEquip(EquipType.Body)]
+	public class RetroVariaSuitBreastplate : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("'Retro' Varia Suit Breastplate");
+
+			SacrificeTotal = 1;
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 18;
+			Item.height = 18;
+			Item.rare = ItemRarityID.Green;
+			Item.vanity = true;
+		}
+		public override bool IsArmorSet(Item head, Item body, Item legs)
+		{
+			return head.type == ModContent.ItemType<RetroVariaSuitHelmet>() && body.type == ModContent.ItemType<RetroVariaSuitBreastplate>() && legs.type == ModContent.ItemType<RetroVariaSuitGreaves>();
+		}
+		public override void UpdateVanitySet(Player P)
+		{
+			MPlayer mp = P.GetModPlayer<MPlayer>();
+			mp.isPowerSuit = true;
+			mp.visorGlowColor = new Color(0, 248, 112);
+			if (P.velocity.Y != 0f && ((P.controlRight && P.direction == 1) || (P.controlLeft && P.direction == -1) || mp.SMoveEffect > 0) && mp.shineDirection == 0 && !mp.shineActive && !mp.ballstate)
+			{
+				mp.jet = true;
+			}
+			else if(mp.shineDirection == 0 || mp.shineDirection == 5)
+			{
+				mp.jet = false;
+			}
+		}
+	}
+	[AutoloadEquip(EquipType.Legs)]
+	public class RetroVariaSuitGreaves : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("'Retro' Varia Suit Greaves");
+
+			SacrificeTotal = 1;
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 18;
+			Item.height = 18;
+			Item.rare = ItemRarityID.Green;
+			Item.vanity = true;
+		}
+	}
+	[AutoloadEquip(EquipType.Head)]
+	public class RetroVariaSuitHelmet : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("'Retro' Varia Suit Helmet");
+
+			SacrificeTotal = 1;
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 18;
+			Item.height = 18;
+			Item.rare = ItemRarityID.Green;
+			Item.vanity = true;
+		}
+	}
+}
