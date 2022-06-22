ac59ff18d7601748d58758029a3a023780a97975
DarkSamus49 <wild.batman@outlook.com>
2022-06-21 21:58:17 -0400

    Change Serris bestiary background

--- Content/NPCs/Serris/Serris_Head.cs
+++ Content/NPCs/Serris/Serris_Head.cs
@@ -101,7 +101,7 @@ namespace MetroidMod.Content.NPCs.Serris
 		{
 			bestiaryEntry.Info.AddRange(new List<IBestiaryInfoElement>
 			{
-				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.TheUnderworld,
+				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.Ocean,
 				new FlavorTextBestiaryInfoElement("An invasive species brought by the Gizzard tribe and released into the seas after the tribe's collapse. The creature moves at extremely high speeds and is hard to keep an eye on. Attacking it will cause it to immediately retaliate and rush into you. Be aware of the creature's speed and strike with a charged attack at the head when it's not moving. Sometimes however… a creature isn't what it appears to be…")
 			});
 		}
