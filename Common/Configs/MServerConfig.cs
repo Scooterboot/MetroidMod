9f1a6e3663a76e4b5ae0834814c9c94a69d030f0
Antinoe <88167211+Antinoe@users.noreply.github.com>
2022-07-06 04:51:37 -0400

    Add files via upload

--- Common/Configs/MServerConfig.cs
+++ Common/Configs/MServerConfig.cs
@@ -25,15 +25,6 @@ namespace MetroidMod.Common.Configs
 		
 		public static MServerConfig Instance;
 		
-		public override bool AcceptClientChanges(ModConfig pendingConfig, int whoAmI, ref string message)
-		{
-			if (Main.netMode != NetmodeID.SinglePlayer)
-			{
-				return whoAmI == 0;
-			}
-			return true;
-		}
-		
 		[Header("General")]
 		
 		[Label("Boss Summon Consumption")]
@@ -61,5 +52,91 @@ namespace MetroidMod.Common.Configs
 			MetroidMod.AutocloseHatchesEnabled = AutocloseHatchesEnabled;
 			MetroidMod.AutocloseHatchesTime = AutocloseHatchesTime;
 		}
+		
+		[Header("Stat Modifiers\n(REQUIRES WORLD RELOAD)")]
+		
+		[Label("[i:MetroidMod/PowerBeam] Power Beam Damage")]
+		[Range(1, 100)]
+		[Increment(1)]
+		[Slider]
+		[DefaultValue(14)]
+		public int damagePowerBeam;
+		
+		[Label("[i:MetroidMod/PowerBeam][i:MetroidMod/ChargeBeamAddon] Power Beam Charge Damage")]
+		[Range(0.05f, 3f)]
+		[Increment(0.05f)]
+		[Slider]
+		[DefaultValue(3f)]
+		public float damageChargePowerBeam;
+		
+		[Label("[i:MetroidMod/PowerBeam] Power Beam Use Time")]
+		[Range(1, 100)]
+		[Increment(1)]
+		[Slider]
+		[DefaultValue(14)]
+		public int useTimePowerBeam;
+		
+		[Label("[i:MetroidMod/PowerBeam] Power Beam Overheat")]
+		[Range(1, 100)]
+		[Increment(1)]
+		[Slider]
+		[DefaultValue(4)]
+		public int overheatPowerBeam;
+		
+		[Label("[i:MetroidMod/MissileLauncher] Missile Launcher Damage")]
+		[Range(1, 100)]
+		[Increment(1)]
+		[Slider]
+		[DefaultValue(30)]
+		public int damageMissileLauncher;
+		
+		[Label("[i:MetroidMod/MissileLauncher] Missile Launcher Starting Ammo")]
+		[Range(1, 100)]
+		[Increment(1)]
+		[Slider]
+		[DefaultValue(5)]
+		public int ammoMissileLauncher;
+		
+		[Label("[i:MetroidMod/MissileExpansion] Ammo per Missile Tank")]
+		[Range(1, 100)]
+		[Increment(1)]
+		[Slider]
+		[DefaultValue(5)]
+		public int ammoMissileTank;
+		
+		[Label("[i:MetroidMod/PowerSuitHelmet] Power Suit Helmet Defense")]
+		[Range(1, 100)]
+		[Increment(1)]
+		[Slider]
+		[DefaultValue(5)]
+		public int defensePowerSuitHelmet;
+		
+		[Label("[i:MetroidMod/PowerSuitBreastplate] Power Suit Breastplate Defense")]
+		[Range(1, 100)]
+		[Increment(1)]
+		[Slider]
+		[DefaultValue(6)]
+		public int defensePowerSuitBreastplate;
+		
+		[Label("[i:MetroidMod/PowerSuitGreaves] Power Suit Greaves Defense")]
+		[Range(1, 100)]
+		[Increment(1)]
+		[Slider]
+		[DefaultValue(5)]
+		public int defensePowerSuitGreaves;
+		
+		[Label("[i:MetroidMod/EnergyTank] Energy Defense Efficiency")]
+		[Range(0.05f, 1f)]
+		[Increment(0.05f)]
+		[Slider]
+		[DefaultValue(0.1f)]
+		public float energyDefenseEfficiency;
+		
+		[Label("[i:MetroidMod/ReserveTank] Energy Expense Efficiency")]
+		[Range(0.05f, 1f)]
+		[Increment(0.05f)]
+		[Slider]
+		[DefaultValue(0.1f)]
+		public float energyExpenseEfficiency;
 	}
 }
