940ab713da80a934c1f6bf1e605ae175f46947a6
DarkSamus49 <wild.batman@outlook.com>
2022-07-27 17:24:07 -0400

    Item Room Tiles

---
+++ Content/Tiles/ItemPedestalTile.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace MetroidMod.Content.Tiles
+{
+	public class ItemPedestalTile : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileMergeDirt[Type] = true;
+			Main.tileBlockLight[Type] = true;
+
+			DustType = 87;
+			MinPick = 65;
+			HitSound = SoundID.Tink;
+			ItemDrop = ModContent.ItemType<Items.Tiles.ItemPedestalTile>();
+
+			AddMapEntry(new Color(187, 187, 206));
+		}
+	}
+}
---
+++ Content/Tiles/ItemRoomTile.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace MetroidMod.Content.Tiles
+{
+	public class ItemRoomTile : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileMergeDirt[Type] = true;
+			Main.tileBlockLight[Type] = true;
+
+			DustType = 87;
+			MinPick = 65;
+			HitSound = SoundID.Tink;
+			ItemDrop = ModContent.ItemType<Items.Tiles.ItemRoomTile>();
+
+			AddMapEntry(new Color(187, 187, 206));
+		}
+	}
+}
