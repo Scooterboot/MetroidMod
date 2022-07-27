430813cbc626a5f0250ec53adfaab35d0637b202
DarkSamus49 <wild.batman@outlook.com>
2022-07-27 16:56:03 -0400

    Arctic and Frosted Arctic tiles

---
+++ Content/Tiles/ArcticPlating.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace MetroidMod.Content.Tiles
+{
+	public class ArcticPlating : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileMergeDirt[Type] = true;
+			Main.tileBlockLight[Type] = true;
+
+			DustType = 87;
+			MinPick = 65;
+			HitSound = SoundID.Tink;
+			ItemDrop = ModContent.ItemType<Items.Tiles.ArcticPlating>();
+
+			AddMapEntry(new Color(140, 189, 189));
+		}
+	}
+}
