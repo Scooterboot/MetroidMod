12055db0b5884d3f75ca469c11e01eaa0ff96173
DarkSamus49 <wild.batman@outlook.com>
2022-08-01 18:34:27 -0400

    Overgrowth Vines

---
+++ Content/Tiles/GreenBrinstarRootsTile.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace MetroidMod.Content.Tiles
+{
+	public class GreenBrinstarRootsTile : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileDungeon[Type] = true;
+			Main.tileBrick[Type] = true;
+			Main.tileLargeFrames[Type] = 1;
+
+			DustType = DustID.Grass;
+			MinPick = 65;
+			HitSound = SoundID.Grass;
+			ItemDrop = ModContent.ItemType<Items.Tiles.GreenBrinstarRootsTile>();
+
+			AddMapEntry(new Color(43, 74, 36));
+		}
+	}
+}
