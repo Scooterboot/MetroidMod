3516d082243654c0a00282d6375ca1d177ca31dd
DarkSamus49 <wild.batman@outlook.com>
2022-08-01 19:25:36 -0400

    Nettori Vegetation

---
+++ Content/Tiles/TanglevineTile.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace MetroidMod.Content.Tiles
+{
+	public class TanglevineTile : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileCut[Type] = true;
+			TileID.Sets.DrawsWalls[Type] = true;
+
+			DustType = DustID.YellowStarDust;
+			HitSound = SoundID.Grass;
+
+			AddMapEntry(new Color(248, 192, 96));
+		}
+	}
+}
