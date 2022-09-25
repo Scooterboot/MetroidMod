085f9bfd16ecd8b5925d40a883455e5dd2e75449
Antinoe <88167211+Antinoe@users.noreply.github.com>
2022-09-25 18:23:20 -0400

    Suit Sounds - Energy: Part 1 (#58)
    * Add files via upload
    
    * [Fix from earlier]
    
    Co-authored-by: DarkSamus49

--- Common/Configs/MConfig.cs
+++ Common/Configs/MConfig.cs
@@ -29,36 +29,63 @@ namespace MetroidMod.Common.Configs
 		{
 		}
 		
+		[Header("General")]
+		
 		[Label("Toggle alternate weapon textures")]
 		[Tooltip("When enabled, shows Metroid Prime style weapons, as opposed to the default Super Metroid style.\n" +
 		"Default value: false")]
 		public bool UseAltWeaponTextures;
-		
+
+		[Label("[i:MetroidMod/EnergyTankAddon] Low Energy Alert")]
+		[Tooltip("When enabled, a beep will be heard when Suit Energy is low.\n" +
+		"Default value: true")]
+		[DefaultValue(true)]
+		public bool energyLow;
+
+        [Label("[i:MetroidMod/EnergyTankAddon] Low Energy Alert Interval")]
+        [Tooltip("The interval between Low Energy beeps.\n[Default: 20]")]
+        [Slider]
+        [DefaultValue(20)]
+        [Range(5, 200)]
+        [Increment(5)]
+        public int energyLowInterval;
+
+		[Label("[i:MetroidMod/EnergyTankAddon] Low Energy Alert Fade")]
+		[Tooltip("When enabled, a fading, non-looping beep will be heard when Suit Energy is low.\n" +
+		"Default value: false\n" + "(WORK IN PROGRESS)")]
+		[DefaultValue(false)]
+		public bool energyLowFade;
+
 		[Header("Draggable Power Beam UI")]
+		
 		[Label("Enabled")]
 		[Tooltip("Allows the Power Beam UI to be draggable.\n" +
 		"Default value: false")]
 		public bool DragablePowerBeamUI;
 		
 		[Header("Draggable Missile Launcher UI")]
+		
 		[Label("Enabled")]
 		[Tooltip("Allows the Missile Launcher UI to be draggable.\n" +
 		"Default value: false")]
 		public bool DragableMissileLauncherUI;
 		
 		[Header("Draggable Morph Ball UI")]
+		
 		[Label("Enabled")]
 		[Tooltip("Allows the Morph Ball UI to be draggable.\n" +
 		"Default value: false")]
 		public bool DragableMorphBallUI;
 		
 		[Header("Draggable Sense Move UI")]
+		
 		[Label("Enabled")]
 		[Tooltip("Allows the Sense Move UI to be draggable.\n" +
 		"Default value: false")]
 		public bool DragableSenseMoveUI;
 
 		[Header("Map Icons")]
+		
 		[Label("[i:MetroidMod/GoldenTorizoSummon] Show Torizo Room Location on Map")]
 		[Tooltip("When enabled, the map will show an icon where Torizo's boss room is.\n" +
 		"Default value: true")]
