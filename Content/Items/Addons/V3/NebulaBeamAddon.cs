3033cc5787d60ce886980c106933b9f8e5ff7b14
Antinoe <88167211+Antinoe@users.noreply.github.com>
2022-10-06 22:21:52 -0400

    Update NebulaBeamAddon.cs

--- Content/Items/Addons/V3/NebulaBeamAddon.cs
+++ Content/Items/Addons/V3/NebulaBeamAddon.cs
@@ -36,9 +36,9 @@ namespace MetroidMod.Content.Items.Addons.V3
 			Item.createTile = ModContent.TileType<Content.Tiles.ItemTile.Beam.NebulaBeamTile>();
 			MGlobalItem mItem = Item.GetGlobalItem<MGlobalItem>();
 			mItem.addonSlotType = 2;
-			mItem.addonDmg = Common.Configs.MConfigItems.Instance.damageIceBeam;
-			mItem.addonHeat = Common.Configs.MConfigItems.Instance.overheatIceBeam;
-			mItem.addonSpeed = Common.Configs.MConfigItems.Instance.speedIceBeam;
+			mItem.addonDmg = Common.Configs.MConfigItems.Instance.damageNebulaBeam;
+			mItem.addonHeat = Common.Configs.MConfigItems.Instance.overheatNebulaBeam;
+			mItem.addonSpeed = Common.Configs.MConfigItems.Instance.speedNebulaBeam;
 		}
 
 		public override void AddRecipes()
