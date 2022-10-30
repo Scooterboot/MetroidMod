9d1ce5067ab7c95301b59f7dd7d109738c0ee1a3
DarkSamus49 <wild.batman@outlook.com>
2022-10-30 05:20:02 -0400

    Totally not forget the debuff for the stungun

--- Content/Buffs/ParalyzerStun.cs
+++ Content/Buffs/ParalyzerStun.cs
@@ -1,1 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+
+namespace MetroidMod.Content.Buffs
+{
+	public class ParalyzerStun : ModBuff
+	{
+		public override string Texture => $"Terraria/Images/Buff_{BuffID.BetsysCurse}";
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Stunned");
+			Description.SetDefault("How did you get this???");
+			Main.debuff[Type] = true;
+			Main.buffNoSave[Type] = true;
+		}
+
+		public override void Update(NPC N, ref int buffIndex)
+		{
+			N.MetroidGlobal().stunned = true;
+			N.MetroidGlobal().speedDecrease = 0;
+		}
 	}
+}
