4b052f6dadf1849651ffa054c6bd48831d37832d
DarkSamus49 <wild.batman@outlook.com>
2023-09-12 11:02:46 -0400

    Change stack sizes

--- Content/Items/Tiles/SerrisTrophy.cs
+++ Content/Items/Tiles/SerrisTrophy.cs
@@ -16,7 +16,7 @@ namespace MetroidMod.Content.Items.Tiles
 		{
 			Item.width = 30;
 			Item.height = 30;
-			Item.maxStack = 99;
+			Item.maxStack = 9999;
 			Item.useTurn = true;
 			Item.autoReuse = true;
 			Item.useAnimation = 15;
