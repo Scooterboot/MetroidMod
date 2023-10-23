7366386ef43453e44b129ddcbc8b70d0228870dc
NPCPotPie <34220670+Piefanatic555@users.noreply.github.com>
2023-10-23 19:04:32 +0900

    Update BeamID.cs
    Further fine-tuning after being re-shaped for future use

--- ID/BeamID.cs
+++ ID/BeamID.cs
@@ -7,81 +7,123 @@ using System.Collections;
 
 namespace MetroidMod.ID
 {
-	public class BeamID
+	public static class BeamID
 	{
-		public enum Charge
-                {
-                        Charge,
-                        ChargeV2,
-                        Luminite,
-                        Hyper,
-                        Phazon
-                }
-                public enum Hunters
-                {
-                        BattleHammer,
-                        Imperialist,
-                        Judicator,
-                        Magmaul,
-                        ShockCoil,
-                        VoltDriver,
-                        OmegaCannon
-                }
-                public enum Utility
-                {
-                        Wave,
-                        WaveV2,
-                        Nebula
-                }
-                public enum PrimaryA
-                {
-                        Spazer,
-                        Wide,
-                        Vortex
-                }
-                public enum PrimaryB
-                {
-                        PlasmaR,
-                        PlasmaG,
-                        Nova,
-                        Solar
-                }
-                public enum Secondary
-                {
-                        Ice,
-                        IceV2,
-                        Stardust
-                }
+		// Can potentially be extended to include Suit or Missile slots.
+		public enum SlotType
+		{
+			Charge,
+			PrimaryA,
+			PrimaryB,
+			Secondary,
+			Utility
+		}
+
+		// Can encompass other addon types by renaming to "Addon". Will potentially lead to a laundry list switch block in getSlotType() and Dictionary below.
+		public enum Beam
+		{
+			Charge,
+			ChargeV2,
+			Luminite,
+			Hyper,
+			Phazon,
+			BattleHammer,
+			Imperialist,
+			Judicator,
+			Magmaul,
+			ShockCoil,
+			VoltDriver,
+			OmegaCannon,
+			Wave,
+			WaveV2,
+			Nebula,
+			Spazer,
+			Wide,
+			Vortex,
+			PlasmaR,
+			PlasmaG,
+			Nova,
+			Solar,
+			Ice,
+			IceV2,
+			Stardust
+		}
+
+		// Will replace BeamAddonSlotID.cs
+		public static int getSlotType(Beam beam)
+		{
+			switch(beam)
+			{
+				case Beam.Charge:
+				case Beam.ChargeV2:
+				case Beam.Luminite:
+				case Beam.Hyper:
+				case Beam.Phazon:
+				case Beam.BattleHammer:
+				case Beam.Imperialist:
+				case Beam.Judicator:
+				case Beam.Magmaul:
+				case Beam.ShockCoil:
+				case Beam.VoltDriver:
+				case Beam.OmegaCannon:
+					return SlotType.Charge;
+				case Beam.Wave:
+				case Beam.WaveV2:
+				case Beam.Nebula:
+					return SlotType.Utility;
+				case Beam.Spazer:
+				case Beam.Wide:
+				case Beam.Vortex:
+					return SlotType.PrimaryA;
+				case Beam.PlasmaR:
+				case Beam.PlasmaG:
+				case Beam.Nova:
+				case Beam.Solar:
+					return SlotType.PrimaryB;
+				case Beam.Ice:
+				case Beam.IceV2:
+				case Beam.Stardust:
+					return SlotType.Secondary;
+				default:
+					return -1;
+			}
+		}
 
 		// This could potentially substantially reduce backend processing time and eventually remove much clutter from PowerBeam.cs
-		public static Dictionary Dict = new HashTable()
+		private static Dictionary<int, int> dict = new HashTable()
 		{	
-			{Charge.Charge, 	ModContent.ItemType<Addons.ChargeBeamAddon>()},
-			{Charge.ChargeV2, 	ModContent.ItemType<Addons.V2.ChargeBeamV2Addon>()},
-			{Charge.Luminite, 	ModContent.ItemType<Addons.V3.LuminiteBeamAddon>()},
-			{Charge.Hyper, 		ModContent.ItemType<Addons.HyperBeamAddon>()},
-			{Charge.Phazon, 	ModContent.ItemType<Addons.PhazonBeamAddon>()},
-			{Hunters.BattleHammer, 	ModContent.ItemType<Addons.Hunters.BattleHammerAddon>()},
-			{Hunters.Imperialist, 	ModContent.ItemType<Addons.Hunters.ImperialistAddon>()},
-			{Hunters.Judicator, 	ModContent.ItemType<Addons.Hunters.JudicatorAddon>()},
-			{Hunters.Magmaul, 	ModContent.ItemType<Addons.Hunters.MagMaulAddon>()},
-			{Hunters.ShockCoil, 	ModContent.ItemType<Addons.Hunters.ShockCoilAddon>()},
-			{Hunters.VoltDriver, 	ModContent.ItemType<Addons.Hunters.VoltDriverAddon>()},
-			{Hunters.OmegaCannon, 	ModContent.ItemType<Addons.Hunters.OmegaCannonAddon>()},
-			{Utility.Wave, 		ModContent.ItemType<Addons.WaveBeamAddon>()},
-			{Utility.WaveV2, 	ModContent.ItemType<Addons.V2.WaveBeamV2Addon>()},
-			{Utility.Nebula, 	ModContent.ItemType<Addons.V3.NebulaBeamAddon>()},
-			{PrimaryA.Spazer, 	ModContent.ItemType<Addons.SpazerAddon>()},
-			{PrimaryA.Wide, 	ModContent.ItemType<Addons.V2.WideBeamAddon>()},
-			{PrimaryA.Vortex, 	ModContent.ItemType<Addons.V3.VortexBeamAddon>()},
-			{PrimaryB.PlasmaR, 	ModContent.ItemType<Addons.PlasmaBeamRedAddon>()},
-			{PrimaryB.PlasmaG, 	ModContent.ItemType<Addons.PlasmaBeamGreenAddon>()},
-			{PrimaryB.Nova, 	ModContent.ItemType<Addons.V2.NovaBeamAddon>()},
-			{PrimaryB.Solar, 	ModContent.ItemType<Addons.V3.SolarBeamAddon>()},
-			{Secondary.Ice, 	ModContent.ItemType<Addons.IceBeamAddon>()},
-			{Secondary.IceV2, 	ModContent.ItemType<Addons.V2.IceBeamV2Addon>()},
-			{Secondary.Stardust, 	ModContent.ItemType<Addons.V3.StardustBeamAddon>()}
+			{Beam.Charge, 		ModContent.ItemType<Addons.ChargeBeamAddon>()},
+			{Beam.ChargeV2, 	ModContent.ItemType<Addons.V2.ChargeBeamV2Addon>()},
+			{Beam.Luminite, 	ModContent.ItemType<Addons.V3.LuminiteBeamAddon>()},
+			{Beam.Hyper, 		ModContent.ItemType<Addons.HyperBeamAddon>()},
+			{Beam.Phazon, 		ModContent.ItemType<Addons.PhazonBeamAddon>()},
+			{Beam.BattleHammer, 	ModContent.ItemType<Addons.Hunters.BattleHammerAddon>()},
+			{Beam.Imperialist, 	ModContent.ItemType<Addons.Hunters.ImperialistAddon>()},
+			{Beam.Judicator, 	ModContent.ItemType<Addons.Hunters.JudicatorAddon>()},
+			{Beam.Magmaul, 		ModContent.ItemType<Addons.Hunters.MagMaulAddon>()},
+			{Beam.ShockCoil, 	ModContent.ItemType<Addons.Hunters.ShockCoilAddon>()},
+			{Beam.VoltDriver, 	ModContent.ItemType<Addons.Hunters.VoltDriverAddon>()},
+			{Beam.OmegaCannon, 	ModContent.ItemType<Addons.Hunters.OmegaCannonAddon>()},
+			{Beam.Wave, 		ModContent.ItemType<Addons.WaveBeamAddon>()},
+			{Beam.WaveV2, 		ModContent.ItemType<Addons.V2.WaveBeamV2Addon>()},
+			{Beam.Nebula, 		ModContent.ItemType<Addons.V3.NebulaBeamAddon>()},
+			{Beam.Spazer, 		ModContent.ItemType<Addons.SpazerAddon>()},
+			{Beam.Wide, 		ModContent.ItemType<Addons.V2.WideBeamAddon>()},
+			{Beam.Vortex, 		ModContent.ItemType<Addons.V3.VortexBeamAddon>()},
+			{Beam.PlasmaR, 		ModContent.ItemType<Addons.PlasmaBeamRedAddon>()},
+			{Beam.PlasmaG, 		ModContent.ItemType<Addons.PlasmaBeamGreenAddon>()},
+			{Beam.Nova, 		ModContent.ItemType<Addons.V2.NovaBeamAddon>()},
+			{Beam.Solar, 		ModContent.ItemType<Addons.V3.SolarBeamAddon>()},
+			{Beam.Ice, 		ModContent.ItemType<Addons.IceBeamAddon>()},
+			{Beam.IceV2, 		ModContent.ItemType<Addons.V2.IceBeamV2Addon>()},
+			{Beam.Stardust, 	ModContent.ItemType<Addons.V3.StardustBeamAddon>()}
 		};
+
+		// Will replace any need for excessive ItemType<> calls
+		public static int getItemTypeOf(Beam beam)
+		{
+			return this.dict[(int) beam];
+		}
 		
 	}
 }
