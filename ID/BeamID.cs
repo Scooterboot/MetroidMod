fa779a4fc25833c04335be5578b3443060f5881d
Piefanatic555 <34220670+Piefanatic555@users.noreply.github.com>
2023-10-23 17:56:25 +0900

    Update BeamID.cs
    Added Enum to ItemType quick converter

--- ID/BeamID.cs
+++ ID/BeamID.cs
@@ -1,4 +1,6 @@
-﻿namespace MetroidMod.ID
+
+
+namespace MetroidMod.ID
 {
 	public class BeamID
 	{
@@ -45,5 +47,35 @@
                         IceV2,
                         Stardust
                 }
+
+		// This could potentially substantially reduce backend processing time and eventually remove much clutter from PowerBeam.cs
+		public static HashTable Dict = new HashTable{
+			{Charge.Charge, 	ModContent.ItemType<Addons.ChargeBeamAddon>()},
+			{Charge.ChargeV2, 	ModContent.ItemType<Addons.V2.ChargeBeamV2Addon>()},
+			{Charge.Luminite, 	ModContent.ItemType<Addons.V3.LuminiteBeamAddon>()},
+			{Charge.Hyper, 		ModContent.ItemType<Addons.HyperBeamAddon>()},
+			{Charge.Phazon, 	ModContent.ItemType<Addons.PhazonBeamAddon>()},
+			{Hunters.BattleHammer, 	ModContent.ItemType<Addons.Hunters.BattleHammerAddon>()},
+			{Hunters.Imperialist, 	ModContent.ItemType<Addons.Hunters.ImperialistAddon>()},
+			{Hunters.Judicator, 	ModContent.ItemType<Addons.Hunters.JudicatorAddon>()},
+			{Hunters.Magmaul, 	ModContent.ItemType<Addons.Hunters.MagMaulAddon>()},
+			{Hunters.ShockCoil, 	ModContent.ItemType<Addons.Hunters.ShockCoilAddon>()},
+			{Hunters.VoltDriver, 	ModContent.ItemType<Addons.Hunters.VoltDriverAddon>()},
+			{Hunters.OmegaCannon, 	ModContent.ItemType<Addons.Hunters.OmegaCannonAddon>()},
+			{Utility.Wave, 		ModContent.ItemType<Addons.WaveBeamAddon>()},
+			{Utility.WaveV2, 	ModContent.ItemType<Addons.V2.WaveBeamV2Addon>()},
+			{Utility.Nebula, 	ModContent.ItemType<Addons.V3.NebulaBeamAddon>()},
+			{PrimaryA.Spazer, 	ModContent.ItemType<Addons.SpazerAddon>()},
+			{PrimaryA.Wide, 	ModContent.ItemType<Addons.V2.WideBeamAddon>()},
+			{PrimaryA.Vortex, 	ModContent.ItemType<Addons.V3.VortexBeamAddon>()},
+			{PrimaryB.PlasmaR, 	ModContent.ItemType<Addons.PlasmaBeamRedAddon>()},
+			{PrimaryB.PlasmaG, 	ModContent.ItemType<Addons.PlasmaBeamGreenAddon>()},
+			{PrimaryB.Nova, 	ModContent.ItemType<Addons.V2.NovaBeamAddon>()},
+			{PrimaryB.Solar, 	ModContent.ItemType<Addons.V3.SolarBeamAddon>()},
+			{Secondary.Ice, 	ModContent.ItemType<Addons.IceBeamAddon>()},
+			{Secondary.IceV2, 	ModContent.ItemType<Addons.V2.IceBeamV2Addon>()},
+			{Secondary.Stardust, 	ModContent.ItemType<Addons.V3.StardustBeamAddon>()}
+		};
+		
 	}
 }
