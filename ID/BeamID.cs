a8f0572a939ec52e3231c87e98b83f0547010a2c
Piefanatic555 <34220670+Piefanatic555@users.noreply.github.com>
2023-10-23 16:54:43 +0900

    Create BeamID.cs
    Creating simpler definition of beam types for use in the long run. Categories can be renamed as required before we begin use.

---
+++ ID/BeamID.cs
@@ -0,0 +1,49 @@
+﻿namespace MetroidMod.ID
+{
+	public class BeamID
+	{
+		public enum Charge
+                {
+                        Charge,
+                        ChargeV2,
+                        Luminite,
+                        Hyper,
+                        Phazon
+                }
+                public enum Hunters
+                {
+                        BattleHammer,
+                        Imperialist,
+                        Judicator,
+                        Magmaul,
+                        ShockCoil,
+                        VoltDriver,
+                        OmegaCannon
+                }
+                public enum Utility
+                {
+                        Wave,
+                        WaveV2,
+                        Nebula
+                }
+                public enum PrimaryA
+                {
+                        Spazer,
+                        Wide,
+                        Vortex
+                }
+                public enum PrimaryB
+                {
+                        PlasmaR,
+                        PlasmaG,
+                        Nova,
+                        Solar
+                }
+                public enum Secondary
+                {
+                        Ice,
+                        IceV2,
+                        Stardust
+                }
+	}
+}
