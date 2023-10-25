021a3c12135d85d96419548eb9abddb85412419c
NPCPotPie <34220670+Piefanatic555@users.noreply.github.com>
2023-10-25 23:48:38 +0900

    Update IBeamAddon.cs
    Probably should have had this variable in the first place

--- Content/Items/Addons/IBeamAddon.cs
+++ Content/Items/Addons/IBeamAddon.cs
@@ -2,6 +2,6 @@ namespace MetroidMod.Content.Items.Addons
 {
         public interface IBeamAddon
         {
-                public int beamID, slotType;
+                public int beamID, slotType, ver;
         }
 }
