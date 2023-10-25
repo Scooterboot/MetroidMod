4e4e762a724d7f2ddaec66fc4bc7f33917e623fa
NPCPotPie <34220670+Piefanatic555@users.noreply.github.com>
2023-10-25 23:57:15 +0900

    Update NovaBeamAddon.cs
    New interface

--- Content/Items/Addons/V2/NovaBeamAddon.cs
+++ Content/Items/Addons/V2/NovaBeamAddon.cs
@@ -1,10 +1,11 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 using MetroidMod.Common.GlobalItems;
+using MetroidMod.ID;
 
 namespace MetroidMod.Content.Items.Addons.V2
 {
-	public class NovaBeamAddon : ModItem
+	public class NovaBeamAddon : ModItem, IBeamAddon
 	{
 		public override void SetStaticDefaults()
 		{
@@ -34,7 +35,9 @@ namespace MetroidMod.Content.Items.Addons.V2
 			Item.consumable = true;
 			Item.createTile = ModContent.TileType<Content.Tiles.ItemTile.Beam.NovaBeamTile>();
 			MGlobalItem mItem = Item.GetGlobalItem<MGlobalItem>();
-			mItem.addonSlotType = 4;
+   			this.beamID = BeamID.Beam.Nova;
+      			this.slotType = BeamID.SlotType.PrimaryB;
+	 		this.ver = 2;
 			mItem.addonDmg = Common.Configs.MConfigItems.Instance.damageNovaBeam;
 			mItem.addonHeat = Common.Configs.MConfigItems.Instance.overheatNovaBeam;
 			mItem.addonSpeed = Common.Configs.MConfigItems.Instance.speedNovaBeam;
