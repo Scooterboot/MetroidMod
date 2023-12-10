7f4cf0d66c02d8ddbb047d69428e7736f1ad23f8
ChaosInsurgent49 <59237646+ChaosInsurgent49@users.noreply.github.com>
2023-12-10 02:56:40 -0500

    Tropic Plating

---
+++ Content/Items/Tiles/TropicPlating.cs
@@ -0,0 +1,107 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MetroidMod.Content.Items.Tiles
+{
+	public class TropicPlating1 : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Item.ResearchUnlockCount = 100;
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 16;
+			Item.height = 16;
+			Item.maxStack = 9999;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.consumable = true;
+			Item.createTile = ModContent.TileType<Content.Tiles.TropicPlating1>();
+		}
+	}
+	public class TropicPlating2 : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Item.ResearchUnlockCount = 100;
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 16;
+			Item.height = 16;
+			Item.maxStack = 9999;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.consumable = true;
+			Item.createTile = ModContent.TileType<Content.Tiles.TropicPlating2>();
+		}
+	}
+	public class TropicPlating3 : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Item.ResearchUnlockCount = 100;
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 16;
+			Item.height = 16;
+			Item.maxStack = 9999;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.consumable = true;
+			Item.createTile = ModContent.TileType<Content.Tiles.TropicPlating3>();
+		}
+	}
+	public class TropicPlating4 : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Item.ResearchUnlockCount = 100;
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 16;
+			Item.height = 16;
+			Item.maxStack = 9999;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.consumable = true;
+			Item.createTile = ModContent.TileType<Content.Tiles.TropicPlating4>();
+		}
+	}
+	public class TropicPlating5 : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Item.ResearchUnlockCount = 100;
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 16;
+			Item.height = 16;
+			Item.maxStack = 9999;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.consumable = true;
+			Item.createTile = ModContent.TileType<Content.Tiles.TropicPlating5>();
+		}
+	}
+}
---
+++ Content/Tiles/TropicPlating.cs
@@ -0,0 +1,88 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace MetroidMod.Content.Tiles
+{
+	public class TropicPlating1 : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileDungeon[Type] = true;
+
+			DustType = 87;
+			MinPick = 65;
+			HitSound = SoundID.Tink;
+			//ItemDrop= ModContent.ItemType<Items.Tiles.TropicPlating1>();
+
+			AddMapEntry(new Color(189, 206, 181));
+		}
+	}
+	public class TropicPlating2 : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileDungeon[Type] = true;
+
+			DustType = 87;
+			MinPick = 65;
+			HitSound = SoundID.Tink;
+			//ItemDrop= ModContent.ItemType<Items.Tiles.TropicPlating2>();
+
+			AddMapEntry(new Color(189, 206, 181));
+		}
+	}
+	public class TropicPlating3 : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileDungeon[Type] = true;
+
+			DustType = 87;
+			MinPick = 65;
+			HitSound = SoundID.Tink;
+			//ItemDrop= ModContent.ItemType<Items.Tiles.TropicPlating3>();
+
+			AddMapEntry(new Color(181, 224, 74));
+		}
+	}
+	public class TropicPlating4 : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileDungeon[Type] = true;
+
+			DustType = 87;
+			MinPick = 65;
+			HitSound = SoundID.Tink;
+			//ItemDrop= ModContent.ItemType<Items.Tiles.TropicPlating4>();
+
+			AddMapEntry(new Color(103, 121, 30));
+		}
+	}
+	public class TropicPlating5 : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileDungeon[Type] = true;
+
+			DustType = 87;
+			MinPick = 65;
+			HitSound = SoundID.Tink;
+			//ItemDrop= ModContent.ItemType<Items.Tiles.TropicPlating5>();
+
+			AddMapEntry(new Color(103, 121, 30));
+		}
+	}
+}
