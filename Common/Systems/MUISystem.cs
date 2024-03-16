54416709041c9790310f2a569267ec37ff308a3c
Zekeram12 <127346846+Zekeram12@users.noreply.github.com>
2024-03-16 14:47:49 -0400

    New Life Reserve UI
    Updated the Life Reserve UI to be compatible with the different Health/Mana styles introduced in 1.4.
    
    Known Issues:
    -Nothing draws over the health bar's "start cap", with the heart logo on it, causing the top of the first reserve bar to be cut off.
    -Some of the drawing for Fancy hearts is a little wonky (thankfully to a degree that's difficult to notice), but I simply just don't know how to change textures that were already drawn.

--- Common/Systems/MUISystem.cs
+++ Common/Systems/MUISystem.cs
@@ -7,6 +7,7 @@ using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Graphics;
 using Terraria;
 using Terraria.Audio;
+using Terraria.GameContent.UI.ResourceSets;
 using Terraria.ID;
 using Terraria.Map;
 using Terraria.ModLoader;
@@ -357,14 +358,14 @@ namespace MetroidMod.Common.Systems
 			int ResourceIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Resource Bars"));
 			if (ResourceIndex != -1)
 			{
-				layers.Insert(ResourceIndex + 1, new LegacyGameInterfaceLayer(
+				/*layers.Insert(ResourceIndex + 1, new LegacyGameInterfaceLayer(
 					"MetroidMod: Reserve Tanks",
 					delegate {
 						DrawReserveHearts(Main.spriteBatch);
 						return true;
 					},
 					InterfaceScaleType.UI)
-				);
+				);*/
 				layers.Insert(ResourceIndex + 2, new LegacyGameInterfaceLayer(
 					"MetroidMod: Suit Energy Bar",
 					delegate {
@@ -828,86 +829,6 @@ namespace MetroidMod.Common.Systems
 					Color s = sjpercent < 1f ? Color.Cyan : Color.SkyBlue;
 					sb.Draw(texBarBorder, new Vector2(x, y), new Rectangle(0, 0, texBarBorder.Width, texBarBorder.Height), Color.White);
 					sb.Draw(texBar, new Vector2(x + 2, y + 2), new Rectangle(0, 0, w, texBar.Height), s);
-				}
-			}
-		}
-		public void DrawReserveHearts(SpriteBatch sb)
-		{
-			Player P = Main.player[Main.myPlayer];
-			MPlayer mp = P.GetModPlayer<MPlayer>();
-			if (mp.reserveTanks > 0)
-			{
-				Texture2D texHeart = ModContent.Request<Texture2D>($"{Mod.Name}/Assets/Textures/ReserveHeart").Value;
-				if (P.whoAmI == Main.myPlayer && P.active && !P.dead && !P.ghost)
-				{
-					float lifePerHeart = 20f;
-					int num = Main.player[Main.myPlayer].statLifeMax / 20;
-					int num2 = (Main.player[Main.myPlayer].statLifeMax - 400) / 5;
-					if (num2 < 0)
-					{
-						num2 = 0;
-					}
-					if (num2 > 0)
-					{
-						num = Main.player[Main.myPlayer].statLifeMax / (20 + num2 / 4);
-						lifePerHeart = (float)Main.player[Main.myPlayer].statLifeMax / 20f;
-					}
-					int num3 = Main.player[Main.myPlayer].statLifeMax2 - Main.player[Main.myPlayer].statLifeMax;
-					lifePerHeart += (float)(num3 / num);
-					int num4 = (int)((float)Main.player[Main.myPlayer].statLifeMax2 / lifePerHeart);
-					if (num4 >= 10)
-					{
-						num4 = 10;
-					}
-					for (int i = 1; i < mp.reserveHearts + 1; i++)
-					{
-						float num5 = 1f;
-						bool flag = false;
-						int num6;
-						if ((float)Main.player[Main.myPlayer].statLife >= (float)i * lifePerHeart)
-						{
-							num6 = 255;
-							if ((float)Main.player[Main.myPlayer].statLife == (float)i * lifePerHeart)
-							{
-								flag = true;
-							}
-						}
-						else
-						{
-							float num7 = ((float)Main.player[Main.myPlayer].statLife - (float)(i - 1) * lifePerHeart) / lifePerHeart;
-							num6 = (int)(30f + 225f * num7);
-							if (num6 < 30)
-							{
-								num6 = 30;
-							}
-							num5 = num7 / 4f + 0.75f;
-							if ((double)num5 < 0.75)
-							{
-								num5 = 0.75f;
-							}
-							if (num7 > 0f)
-							{
-								flag = true;
-							}
-						}
-						if (flag)
-						{
-							num5 += Main.cursorScale - 1f;
-						}
-						int num8 = 0;
-						int num9 = 0;
-						if (i > 10)
-						{
-							num8 -= 260;
-							num9 += 26;
-						}
-						int a = (int)((double)((float)num6) * 0.9);
-						if (mp.reserveHeartsValue >= 25)
-						{
-							texHeart = ModContent.Request<Texture2D>($"{Mod.Name}/Assets/Textures/ReserveHeart2").Value;
-						}
-						sb.Draw(texHeart, new Vector2((float)(500 + 26 * (i - 1) + num8 + (Main.screenWidth - 800) + Terraria.GameContent.TextureAssets.Heart.Value.Width / 2), 32f + ((float)Terraria.GameContent.TextureAssets.Heart.Value.Height - (float)Terraria.GameContent.TextureAssets.Heart.Value.Height * num5) / 2f + (float)num9 + (float)(Terraria.GameContent.TextureAssets.Heart.Value.Height / 2)), new Rectangle?(new Rectangle(0, 0, texHeart.Width, texHeart.Height)), new Color(num6, num6, num6, a), 0f, new Vector2((float)(texHeart.Width / 2), (float)(texHeart.Height / 2)), num5, SpriteEffects.None, 0f);
-					}
 				}
 			}
 		}
