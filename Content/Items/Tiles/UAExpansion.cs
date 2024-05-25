b3e7144ae947f33afa75b18b4e94f7c6a627d8db
ChaosInsurgent49 <59237646+ChaosInsurgent49@users.noreply.github.com>
2024-05-24 22:21:26 -0400

    Fix UA Expansions shimmering to the wrong Energy Tank

--- Content/Items/Tiles/UAExpansion.cs
+++ Content/Items/Tiles/UAExpansion.cs
@@ -14,7 +14,7 @@ namespace MetroidMod.Content.Items.Tiles
 			/* Tooltip.SetDefault("A Missile Expansion\n" +
 				"Increase maximum Missiles by 5 with each expansion slotted in\n" +
 				"Stack it up to 50 expansions for +250 maximum Missiles"); */
-			ItemID.Sets.ShimmerTransformToItem[Type] = ModContent.ItemType<EnergyTank>();
+			ItemID.Sets.ShimmerTransformToItem[Type] = SuitAddonLoader.GetAddon<SuitAddons.EnergyTank>().ItemType;
 			Item.ResearchUnlockCount = 12;
 		}
 
