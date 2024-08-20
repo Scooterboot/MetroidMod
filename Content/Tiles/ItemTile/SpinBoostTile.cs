b856f63bf93425d322a83e65d2ba5b49f409277c
Zekeram12 <127346846+Zekeram12@users.noreply.github.com>
2024-08-20 11:50:22 -0400

    Spin Boost Test
    Attempting to add the Spin Boost, semi-replacing the Space Jump Boots as a test. The Space Jump Boots can still be obtained by shimmering the Spin Boost.
    
    Currently having issues on my end though so dag nabbit and all that

---
+++ Content/Tiles/ItemTile/SpinBoostTile.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MetroidMod.Content.Tiles.ItemTile
+{
+	public class SpinBoostTile : ItemTile
+	{
+		public override void SetStaticDefaults()
+		{
+			base.SetStaticDefaults();
+			AddMapEntry(new Color(254, 159, 25));
+			//ItemDrop = ModContent.ItemType<Items.Accessories.SpinBoost>();
+			DustType = 1;
+		}
+	}
+}
