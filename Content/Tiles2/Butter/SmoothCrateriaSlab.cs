c2bc716bd1fe703c707d796f1f69543c15b66530
Nov <28666422+EleventhNov@users.noreply.github.com>
2024-08-21 02:21:20 -0500

    Rename SmoothCrateria to SmoothCrateriaSlab

--- Content/Tiles2/Butter/SmoothCrateriaSlab.cs
+++ Content/Tiles2/Butter/SmoothCrateriaSlab.cs
@@ -4,7 +4,7 @@ using Terraria.ID;
 
 namespace MetroidMod.Content.Tiles2.Butter
 {
-	internal class SmoothCrateria : GenericTile
+	internal class SmoothCrateriaSlab : GenericTile
 	{
 		public override Color MapColor => Color.DarkCyan;
 		public override SoundStyle HitSound => SoundID.Tink;
