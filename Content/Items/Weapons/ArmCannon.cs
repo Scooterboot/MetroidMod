d171f95d23c32f31d1465981e98603494097c677
Nov <28666422+EleventhNov@users.noreply.github.com>
2024-08-22 21:28:01 -0500

    Fix player holding onto Arm Cannon while performing a missile combo even if they run out of missiles

--- Content/Items/Weapons/ArmCannon.cs
+++ Content/Items/Weapons/ArmCannon.cs
@@ -2464,10 +2464,6 @@ namespace MetroidMod.Content.Items.Weapons
 											}
 										}
 									}
-									else
-									{
-										Main.projectile[chargeLead].Kill();
-									}
 								}
 							}
 							else
@@ -2676,9 +2672,17 @@ namespace MetroidMod.Content.Items.Weapons
 						targetNum = 0;
 						targetingDelay = 0;
 					}
-					if(pb.statMissiles <= 0f && player.controlUseItem)
+
+					bool ranOutOfMissiles = pb.statMissiles <= 0f;
+					if (ranOutOfMissiles)
 					{
-						pb.isBeam = !pb.isBeam;
+						Main.projectile[chargeLead].Kill();
+
+						bool tryingToUseLauncher = Main.mouseLeft && Main.mouseLeftRelease;
+						if (tryingToUseLauncher)
+						{
+							pb.isBeam = true;
+						}
 					}
 				}
 
