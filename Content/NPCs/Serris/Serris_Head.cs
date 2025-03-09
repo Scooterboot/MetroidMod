3da02b2337e4839e123dd4533d60b6c9f1e43149
Zekeram12 <127346846+Zekeram12@users.noreply.github.com>
2025-03-09 13:48:42 -0400

    Serris damage nerf
    Nerfed Serris' base damage values substantially.
    Normal damage: 40 -> 24
    Boosting damage: 120 -> 72
    Core damage: 60 -> 30
    (Seriously, over a hundred damage in CLASSIC mode???)

--- Content/NPCs/Serris/Serris_Head.cs
+++ Content/NPCs/Serris/Serris_Head.cs
@@ -49,9 +49,9 @@ namespace MetroidMod.Content.NPCs.Serris
 			set { NPC.ai[1] = value; }
 		}
 
-		int damage = 40;//20
-		int speedDamage = 120;//60;//35
-		int coreDamage = 60;//30
+		int damage = 24;//20
+		int speedDamage = 72;//60;//35
+		int coreDamage = 30;//30
 
 		public override void SetStaticDefaults()
 		{
