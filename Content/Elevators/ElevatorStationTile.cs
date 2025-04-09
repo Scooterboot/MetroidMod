945cf65d9078fa081afe44552a74f2f06fd52f45
Nov <28666422+EleventhNov@users.noreply.github.com>
2025-04-09 14:50:09 -0500

    Add elevator station tile

---
+++ Content/Elevators/ElevatorStationTile.cs
@@ -0,0 +1,31 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MetroidMod.Content.Elevators
+{
+	internal class ElevatorStationTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = false;
+            Main.tileFrameImportant[Type] = true;
+
+            DustType = DustID.Stone;
+            AddMapEntry(new(200, 200, 200));
+
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.Width = 4;
+            TileObjectData.newTile.Height = 1;
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinateHeights = [16, 16];
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.addTile(Type);
+        }
+    }
+}
