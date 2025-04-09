614b4c1ec5702dfc4903aaae5ce16a44959e183d
Nov <28666422+EleventhNov@users.noreply.github.com>
2025-04-09 17:35:41 -0500

    Show player behind blocks when using elevator

---
+++ ListExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MetroidMod
+{
+	internal static class ListExtensions
+	{
+		/// <summary>
+		/// Remove all the elements of the list that match the predicate,
+		/// and return them in a separate list.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="list"></param>
+		/// <param name="predicate"></param>
+		/// <returns></returns>
+		public static List<T> RemoveWhere<T>(this List<T> list, Func<T, bool> predicate)
+		{
+			List<T> removed = new();
+			for (int i = list.Count - 1; i >= 0; i--)
+			{
+				if (predicate(list[i]))
+				{
+					removed.Add(list[i]);
+					list.RemoveAt(i);
+				}
+			}
+			return removed;
+		}
+	}
+}
