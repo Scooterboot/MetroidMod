901f90b8e62a1a63361b14ea72b539b6592617e4
Nov <28666422+EleventhNov@users.noreply.github.com>
2025-04-09 21:45:44 -0500

    Add pickaxe requirement and map color to elevators

--- Content/Elevators/TopElevatorStationTile.cs
+++ Content/Elevators/TopElevatorStationTile.cs
@@ -1,33 +1,11 @@
-﻿using Terraria.ID;
-using Terraria;
-using Terraria.ModLoader;
-using Terraria.ObjectData;
+﻿
+using Terraria.DataStructures;
 
-namespace MetroidMod.Content.Elevators
-{
-	internal class TopElevatorStationTile : ModTile
+namespace MetroidMod.Content.Elevators{
+	internal class TopElevatorStationTile : ElevatorStationTile
 	{
-		public override void SetStaticDefaults()
-		{
-			Main.tileSolid[Type] = true;
-			Main.tileMergeDirt[Type] = true;
-			Main.tileBlockLight[Type] = false;
-			Main.tileFrameImportant[Type] = true;
-
-			DustType = DustID.Stone;
-			AddMapEntry(new(200, 200, 200));
-
-			TileObjectData.newTile.UsesCustomCanPlace = true;
-			TileObjectData.newTile.StyleHorizontal = false;
-			TileObjectData.newTile.Width = 4;
-			TileObjectData.newTile.Height = 1;
-			TileObjectData.newTile.CoordinateWidth = 16;
-			TileObjectData.newTile.CoordinateHeights = [16, 16];
-			TileObjectData.newTile.CoordinatePadding = 2;
-			TileObjectData.newTile.Origin = new(1, 0);
-			TileObjectData.addTile(Type);
-
-			AnimationFrameHeight = 18;
-		}
+		public override bool Animated => false;
+		public override int Height => 1;
+		public override Point16 Origin => new(1, 0);
 	}
 }
