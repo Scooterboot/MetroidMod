324f646c2694ee9d13a122906748320fd7809482
Nov <28666422+EleventhNov@users.noreply.github.com>
2025-04-09 21:45:44 -0500

    Add pickaxe requirement and map color to elevators

--- Content/Elevators/ElevatorStationTile.cs
+++ Content/Elevators/ElevatorStationTile.cs
@@ -2,12 +2,20 @@
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
+using System.Linq;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
 
 namespace MetroidMod.Content.Elevators
 {
 	internal class ElevatorStationTile : ModTile
     {
-        public override void SetStaticDefaults()
+		public virtual int Height => 2;
+		public virtual bool Animated => false;
+		public virtual int FrameAmount => 3;
+		public virtual Point16 Origin => new(1, 1);
+
+		public override void SetStaticDefaults()
         {
             Main.tileSolid[Type] = true;
             Main.tileMergeDirt[Type] = true;
@@ -15,27 +23,29 @@ namespace MetroidMod.Content.Elevators
             Main.tileFrameImportant[Type] = true;
 
             DustType = DustID.Stone;
-            AddMapEntry(new(200, 200, 200));
+			MinPick = 100;
+            AddMapEntry(Color.Yellow);
 
             TileObjectData.newTile.UsesCustomCanPlace = true;
             TileObjectData.newTile.StyleHorizontal = false;
             TileObjectData.newTile.Width = 4;
-            TileObjectData.newTile.Height = 2;
+            TileObjectData.newTile.Height = Height;
             TileObjectData.newTile.CoordinateWidth = 16;
-            TileObjectData.newTile.CoordinateHeights = [16, 16];
+            TileObjectData.newTile.CoordinateHeights = Enumerable.Repeat(16, Height).ToArray();
             TileObjectData.newTile.CoordinatePadding = 2;
-			TileObjectData.newTile.Origin = new(1, 1);
+			TileObjectData.newTile.Origin = Origin;
             TileObjectData.addTile(Type);
 
-			AnimationFrameHeight = 36;
+			AnimationFrameHeight = TileObjectData.newTile.CoordinateFullHeight;
         }
 
 		public override void AnimateTile(ref int frame, ref int frameCounter)
 		{
+			if (!Animated) return;
 			if(++frameCounter >= 8)
 			{
 				frameCounter = 0;
-				frame = (++frame) % 3;
+				frame = (++frame) % FrameAmount;
 			}
 		}
 	}
