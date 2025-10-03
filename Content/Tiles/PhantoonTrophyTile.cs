415b856cda7099c3b20a0098c6a87184359cf39a
Joost8910 <bluedog@fenria.com>
2025-10-02 23:55:34 -0500

    Phantoon Trophy haunted eyeball

--- Content/Tiles/PhantoonTrophyTile.cs
+++ Content/Tiles/PhantoonTrophyTile.cs
@@ -1,7 +1,11 @@
 using System;
+using MetroidMod.Content.NPCs.Mobs.Crawler;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 using Terraria;
 using Terraria.DataStructures;
+using Terraria.GameContent.Drawing;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
@@ -11,6 +15,7 @@ namespace MetroidMod.Content.Tiles
 {
 	public class PhantoonTrophyTile : ModTile
 	{
+		public static Asset<Texture2D> pupilTexture => ModContent.Request<Texture2D>("MetroidMod/Content/Tiles/PhantoonTrophyTile_Eye");
 		public override void SetStaticDefaults()
 		{
 			Main.tileFrameImportant[Type] = true;
@@ -30,6 +35,53 @@ namespace MetroidMod.Content.Tiles
 		public override void KillMultiTile(int i, int j, int frameX, int frameY)
 		{
 			new EntitySource_TileBreak(i, j); //Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 48, 48, ModContent.ItemType<Items.Tiles.PhantoonTrophy>());
+		}
+		public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+		{
+			Tile tile = Main.tile[i, j];
+			if (tile.TileFrameX > 18 && tile.TileFrameY > 18)
+			{
+				i--;
+				j--;
+			}
+			else
+			{
+				return;
+			}
+			if (!TileDrawing.IsVisible(Main.tile[i, j])) { return; } //Don't do any of this if the tile's invisible (i.e. echo paint)
+
+			Vector2 zero = new Vector2(Main.offScreenRange, Main.offScreenRange);
+				if (Main.drawToScreen)
+				{
+					zero = Vector2.Zero;
+				}
+				Vector2 eyeDistance = new Vector2(Main.LocalPlayer.Center.X - i * 16, Main.LocalPlayer.Center.Y - j * 16);
+				Vector2 eyeAim = Vector2.Zero;
+
+				if (eyeDistance.Length() > 450)
+				{
+					int c = 50;
+					if (eyeDistance.X < -c)
+					{
+						eyeAim.X = -2;
+					}
+					if (eyeDistance.X > c)
+					{
+						eyeAim.X = 2;
+					}
+					if (eyeDistance.Y < -c)
+					{
+						eyeAim.Y = -2;
+					}
+					if (eyeDistance.Y > c)
+					{
+						eyeAim.Y = 2;
+					}
+				}
+				Rectangle pupilRectangle = pupilTexture.Frame();
+				Vector2 offset = new Vector2(2, -4) + eyeAim;
+				spriteBatch.Draw(pupilTexture.Value, new Vector2(i * 16 - (int)Main.screenPosition.X, j * 16 - (int)Main.screenPosition.Y) + zero + offset, pupilRectangle, Color.White);
+			
 		}
 
 		//public override void AnimateTile(ref int frame, ref int frameCounter)
