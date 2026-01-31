56c0c2be05a2269afa8be2b88573e9c3011657e1
Joost8910 <bluedog@fenria.com>
2026-01-30 19:45:00 -0600

    Alpha Metroid Charge Attack

--- Content/NPCs/Metroids/Alpha/AlphaMetroid.cs
+++ Content/NPCs/Metroids/Alpha/AlphaMetroid.cs
@@ -9,6 +9,7 @@ using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using Terraria.WorldBuilding;
 using static MetroidMod.Sounds;
 
 namespace MetroidMod.Content.NPCs.Metroids.Alpha
@@ -46,12 +47,29 @@ namespace MetroidMod.Content.NPCs.Metroids.Alpha
 			{
 				AI_Counter = 100;
 			}
+
+			if (STATE == (int)StateID.Aggroed && AI_Substate == (int)Aggro_Substate.Charge) //Melee Counter during the charge
+			{
+				if (AI_Counter > 60 && AI_Counter < 84)
+				{
+					AI_Substate = (int)Aggro_Substate.Stun;
+					AI_Counter = 90; //Stun Duration
+				}
+			}
 		}
 		public override void OnHitByProjectile(Projectile projectile, NPC.HitInfo hit, int damageDone)
 		{
 			if (STATE == (int)StateID.Coccoon && AI_Counter < 100)
 			{
 				AI_Counter = 100;
+			}
+			if (STATE == (int)StateID.Aggroed && AI_Substate == (int)Aggro_Substate.Charge) //Melee Counter during the charge
+			{
+				if (Main.player[projectile.owner].heldProj == projectile.whoAmI && AI_Counter > 60 && AI_Counter < 84)
+				{
+					AI_Substate = (int)Aggro_Substate.Stun;
+					AI_Counter = 90; //Stun Duration
+				}
 			}
 		}
 		public override bool CanHitPlayer(Player target, ref int cooldownSlot)
@@ -72,6 +90,21 @@ namespace MetroidMod.Content.NPCs.Metroids.Alpha
 		}
 		public override void ModifyHitByItem(Player player, Item item, ref NPC.HitModifiers modifiers)
 		{
+			if (STATE == (int)StateID.Aggroed && AI_Substate == (int)Aggro_Substate.Charge)
+			{
+				if (AI_Counter > 60 && AI_Counter <= 85)
+				{
+					modifiers.SetCrit();
+					modifiers.ArmorPenetration += 370;
+					modifiers.Knockback += 5;
+					SoundEngine.PlaySound(SoundID.NPCHit18, NPC.Center);
+					return;
+				}
+				else
+				{
+					modifiers.DisableKnockback();
+				}
+			}
 			if (STATE == (int)StateID.Coccoon)
 			{
 				modifiers.DisableKnockback();
@@ -93,6 +126,20 @@ namespace MetroidMod.Content.NPCs.Metroids.Alpha
 		}
 		public override void ModifyHitByProjectile(Projectile projectile, ref NPC.HitModifiers modifiers)
 		{
+			if (STATE == (int)StateID.Aggroed && AI_Substate == (int)Aggro_Substate.Charge)
+			{
+				if (Main.player[projectile.owner].heldProj == projectile.whoAmI && AI_Counter > 60 && AI_Counter <= 85)
+				{
+					modifiers.SetCrit();
+					modifiers.ArmorPenetration += 370;
+					SoundEngine.PlaySound(SoundID.NPCHit18, NPC.Center);
+					return;
+				}
+				else
+				{
+					modifiers.DisableKnockback();
+				}
+			}
 			if (STATE == (int)StateID.Coccoon)
 			{
 				modifiers.DisableKnockback();
@@ -122,12 +169,21 @@ namespace MetroidMod.Content.NPCs.Metroids.Alpha
 		}
 		public ref float STATE => ref NPC.ai[0];
 		public ref float AI_Counter => ref NPC.ai[1];
+		public ref float AI_Substate => ref NPC.ai[2];
 		public ref float Wiggle => ref NPC.localAI[0];
 		private enum StateID : int
 		{
 			Coccoon,
 			Idle,
-			Aggroed
+			Aggroed,
+			JustHatched
+		}
+		private enum Aggro_Substate : int
+		{
+			Neutral,
+			Charge,
+			Sparks,
+			Stun
 		}
 		public override void AI()
 		{
@@ -170,7 +226,8 @@ namespace MetroidMod.Content.NPCs.Metroids.Alpha
 				if (AI_Counter >= 300)
 				{
 					AI_Counter = 0;
-					STATE = (int)StateID.Aggroed;
+					STATE = (int)StateID.JustHatched;
+					NPC.noGravity = true;
 					NPC.velocity.Y = -6;
 					int index = Item.NewItem(NPC.GetSource_FromThis(), NPC.Center + new Vector2(-4, 0), Vector2.Zero, ModContent.ItemType<HuskLarva>());
 					Main.item[index].velocity = new Vector2(0, -1);
@@ -185,11 +242,243 @@ namespace MetroidMod.Content.NPCs.Metroids.Alpha
 					Gore.NewGore(NPC.GetSource_FromThis(), NPC.Center + new Vector2(3, -4), NPC.velocity, Mod.Find<ModGore>("AlphaCoccoonShard4").Type, NPC.scale);
 				}
 			}
+			if (STATE == (int)StateID.JustHatched)
+			{
+				NPC.noGravity = true;
+				AI_Counter++;
+				NPC.velocity *= 0.95f;
+				if (AI_Counter >= 60)
+				{
+					AI_Counter = 0;
+					STATE = (int)StateID.Aggroed;
+				}
+			}
 			if (STATE == (int)StateID.Aggroed)
 			{
 				NPC.noGravity = true;
-				NPC.velocity *= 0.95f;
-				NPC.rotation += MathHelper.ToRadians(0.5f);
+				//NPC.velocity *= 0.95f;
+				//NPC.rotation += MathHelper.ToRadians(0.5f);
+				Vector2 targetVelocity = Vector2.Zero;
+				if (!NPC.HasValidTarget)
+				{
+					STATE = (int)StateID.Idle;
+					NPC.netUpdate = true;
+				}
+				else
+				{
+					Player p = Main.player[NPC.target];
+					NPC.targetRect = p.Hitbox;
+					targetVelocity = p.velocity;
+				}
+
+				if (AI_Substate == (int)Aggro_Substate.Neutral)
+				{
+					AI_Counter++;
+
+					Vector2 accel = new Vector2(0.2f, 0.15f);
+					Vector2 speed = new Vector2(7.5f, 5f);
+
+					Vector2 targetPos = new Vector2(NPC.targetRect.Center.X, NPC.targetRect.Center.Y - 120) + targetVelocity * 20;
+					if (NPC.Center.X > NPC.targetRect.Center.X)
+					{
+						targetPos.X += 160;
+					}
+					else
+					{
+						targetPos.X -= 160;
+					}
+					NPC.direction = Math.Sign(NPC.DirectionTo(targetPos).X);
+					NPC.directionY = Math.Sign(NPC.DirectionTo(targetPos).Y);
+					if (AI_Counter > 150)
+					{
+						speed *= Math.Min(1.5f, AI_Counter / 150);
+						accel *= Math.Min(1.5f, AI_Counter / 150);
+					}
+
+					if (NPC.Distance(targetPos) < 40)
+					{
+						AI_Counter = 0;
+						AI_Substate = (int)Aggro_Substate.Charge;
+					}
+					else 
+					{
+						//Main.NewText(Math.Abs(NPC.Distance(targetPos)));
+						if (NPC.Distance(targetPos) < 60)
+						{
+							speed.Y *= NPC.Distance(targetPos) / 60;
+						}
+					}
+
+					if (NPC.velocity.X * NPC.direction < speed.X)
+					{
+						NPC.velocity.X += NPC.direction * accel.X;
+					}
+					else
+					{
+						NPC.velocity.X = speed.X * NPC.direction;
+					}
+					if (NPC.direction * Math.Sign(NPC.velocity.X) > 0)
+					{
+						NPC.spriteDirection = NPC.direction;
+					}
+
+					if (NPC.velocity.Y * NPC.directionY < speed.Y)
+					{
+						NPC.velocity.Y += NPC.directionY * accel.Y;
+					}
+					else
+					{
+						NPC.velocity.Y = speed.Y * NPC.directionY;
+					}
+
+					if (AI_Counter >= 250)
+					{
+					}
+				}
+
+				if (AI_Substate == (int)Aggro_Substate.Charge)
+				{
+					AI_Counter++;
+					if (AI_Counter == 1)
+					{
+						NPC.FaceTarget();
+						NPC.spriteDirection = NPC.direction;
+					}
+					float speed = 16.5f;
+					float rot = NPC.direction < 0 ? MathHelper.ToRadians(110) : MathHelper.ToRadians(70);
+					Vector2 targetPos = new Vector2(NPC.targetRect.Center.X, NPC.targetRect.Center.Y);
+					if (Main.expertMode)
+					{
+						Vector2 trajectory = NPC.DirectionTo(targetPos);
+						rot = trajectory.ToRotation() + MathHelper.ToRadians(35f) * NPC.direction;
+					}
+					if (AI_Counter < 60)
+					{
+						if (AI_Counter < 25)
+						{
+							NPC.velocity.X = -1.25f * NPC.direction;
+							NPC.velocity.Y = -1.25f;
+							//NPC.rotation += MathHelper.ToRadians(3f) * NPC.direction;
+							NPC.rotation = (NPC.direction < 0 ? rot - MathHelper.Pi : rot) * (AI_Counter / 25);
+						}
+						else
+						{
+							NPC.rotation = (NPC.direction < 0 ? rot - MathHelper.Pi : rot);
+							NPC.velocity *= 0.92f;
+							if (Wiggle > 0)
+							{
+								Wiggle--;
+							}
+							if (AI_Counter == 40)
+							{
+								Wiggle = 16;
+							}
+						}
+					}
+					if (AI_Counter == 60)
+					{
+						//NPC.velocity.Y = 16;
+						//NPC.velocity.X = 4 * NPC.direction;
+						//NPC.velocity = trajectory.RotatedBy(MathHelper.ToRadians(35f) * NPC.direction);
+						NPC.velocity = rot.ToRotationVector2() * speed;
+
+						NPC.rotation = NPC.velocity.ToRotation() + (NPC.direction < 0 ? MathHelper.Pi : 0);
+					}
+					bool passedTarget = NPC.DirectionTo(targetPos).X * NPC.velocity.X < 0;
+					if (AI_Counter > 60 && AI_Counter <= 84)
+					{
+						//NPC.velocity = NPC.velocity.RotatedBy(MathHelper.ToRadians(-4.5f) * NPC.direction);
+						Vector2 projection = NPC.Center + (NPC.Distance(targetPos) / speed) * NPC.velocity;
+						if (projection.Y > targetPos.Y || passedTarget || (NPC.Center.Y < targetPos.Y && NPC.velocity.Y < 0))
+						{
+							NPC.rotation += MathHelper.ToRadians(-4.5f) * NPC.direction;
+						}
+						NPC.velocity = NPC.rotation.ToRotationVector2() * NPC.direction * speed;
+					}
+					if (AI_Counter >= 85)
+					{
+						if (passedTarget || AI_Counter >= 100)
+						{
+							NPC.velocity *= 0.9f;
+						}
+					}
+					if (AI_Counter >= 105)
+					{
+						NPC.rotation = MathHelper.WrapAngle(NPC.rotation) * 0.9f;
+					}
+					if (AI_Counter >= 125)
+					{
+						AI_Substate = (int)Aggro_Substate.Neutral;
+						AI_Counter = 0;
+						NPC.rotation = 0f;
+					}
+				}
+
+				if (AI_Substate == (int)Aggro_Substate.Stun)
+				{
+					AI_Counter--;
+					if (AI_Counter > 45)
+					{
+						float rotSpeed = 2 + (AI_Counter / 6);
+						NPC.rotation += MathHelper.ToRadians(-rotSpeed) * NPC.direction;
+						NPC.velocity *= 0.96f;
+					}
+					else
+					{
+						NPC.velocity *= 0.9f;
+						NPC.rotation = MathHelper.WrapAngle(NPC.rotation) * 0.92f;
+					}
+					if (AI_Counter <= 0)
+					{
+						AI_Substate = (int)Aggro_Substate.Neutral;
+						NPC.rotation = 0;
+					}
+				}
+			}
+		}
+		public override void FindFrame(int frameHeight)
+		{
+			if (STATE == (int)StateID.Aggroed)
+			{
+				if (AI_Substate == (int)Aggro_Substate.Charge)
+				{
+					if (AI_Counter > 10 && AI_Counter < 60)
+					{
+						if (NPC.frame.Y < 2 * frameHeight)
+						{
+							NPC.frameCounter++;
+							if (NPC.frameCounter > 6)
+							{
+								NPC.frameCounter = 0;
+								NPC.frame.Y += frameHeight;
+							}
+						}
+					}
+					if (AI_Counter > 85)
+					{
+						if (NPC.frame.Y > 0)
+						{
+							NPC.frameCounter++;
+							if (NPC.frameCounter > 6)
+							{
+								NPC.frameCounter = 0;
+								NPC.frame.Y -= frameHeight;
+							}
+						}
+					}
+				}
+				if (AI_Substate == (int)Aggro_Substate.Stun)
+				{
+					if (NPC.frame.Y > 0)
+					{
+						NPC.frameCounter++;
+						if (NPC.frameCounter > 6)
+						{
+							NPC.frameCounter = 0;
+							NPC.frame.Y -= frameHeight;
+						}
+					}
+				}
 			}
 		}
 		public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
