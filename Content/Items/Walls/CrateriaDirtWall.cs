24e722bea9556354c0a9cc479abf4c517cf6ec8d
DrMetroid <120794791+DrMetroid@users.noreply.github.com>
2026-05-22 13:29:39 -0600

    walls

--- Content/Items/Walls/CrateriaDirtWall.cs
+++ Content/Items/Walls/CrateriaDirtWall.cs
@@ -14,16 +14,7 @@ namespace MetroidMod.Content.Items.Walls
 		}
 		public override void SetDefaults()
 		{
-			Item.width = 16;
-			Item.height = 16;
-			Item.maxStack = 9999;
-			Item.useTurn = true;
-			Item.autoReuse = true;
-			Item.useAnimation = 15;
-			Item.useTime = 10;
-			Item.useStyle = ItemUseStyleID.Swing;
-			Item.consumable = true;
-			Item.createWall = ModContent.WallType<Content.Walls.CrateriaDirtWall>();
+			Item.DefaultToPlaceableWall(ModContent.WallType<Content.Walls.CrateriaDirtWall>());
 		}
 	}
 }
--- Content/Items/Walls/NorfairBrickWallNatural.cs
+++ Content/Items/Walls/NorfairBrickWallNatural.cs
@@ -16,17 +16,8 @@ namespace MetroidMod.Content.Items.Walls
 		}
 		public override void SetDefaults()
 		{
-			Item.width = 16;
-			Item.height = 16;
-			Item.maxStack = 9999;
-			Item.useTurn = true;
-			Item.autoReuse = true;
-			Item.useAnimation = 15;
-			Item.useTime = 10;
-			Item.useStyle = ItemUseStyleID.Swing;
-			Item.consumable = true;
 			ItemID.Sets.DrawUnsafeIndicator[Item.type] = true; //Hey so apparently they just have a thingy to make the unsafe skull show up.    -Z
-			Item.createWall = ModContent.WallType<Content.Walls.NorfairBrickWallNatural>();
+			Item.DefaultToPlaceableWall(ModContent.WallType<Content.Walls.NorfairBrickWallNatural>());
 		}
 	}
 }
