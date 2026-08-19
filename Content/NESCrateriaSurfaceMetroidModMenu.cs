5672f7f73bd107f67c6a8e83afeca521761e0ece
Armipotent <59237646+Armipotent@users.noreply.github.com>
2026-08-19 13:42:02 -0600

    NES title screen touch up, part 1

--- Content/NESCrateriaSurfaceMetroidModMenu.cs
+++ Content/NESCrateriaSurfaceMetroidModMenu.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using Terraria;
@@ -14,31 +15,63 @@ namespace MetroidMod.Content
 		public override ModSurfaceBackgroundStyle MenuBackgroundStyle => ModContent.GetInstance<Biomes.NESCrateriaSurfaceBackgroundStyle>();
 		public override Asset<Texture2D> MoonTexture => ModContent.Request<Texture2D>($"{Mod.Name}/Assets/Textures/Pixel");
 		public override Asset<Texture2D> SunTexture => ModContent.Request<Texture2D>($"{Mod.Name}/Assets/Textures/Pixel");
+		public Asset<Texture2D> LogoGlint => ModContent.Request<Texture2D>($"{Mod.Name}/Assets/Textures/Menu/NESTitleGlint");
 
-		public override string DisplayName => "Crateria (Surface, Metroid NES)";
+		public override string DisplayName => Mod.GetLocalization("Menus.NES", PrettyPrintName).Value;
 
 		/*public override void OnSelected()
 		{
 			//SoundEngine.PlaySound(SoundID.GuitarC);
 		}*/
+		
+		internal int tick = 0;
+		internal static readonly int tickMax = 500;
+		internal static readonly int tickThresholdForGlint = 400;
+		internal float glintPercent = 0;
+
+
+		public override void Update(bool isOnTitleScreen)
+		{
+			if (Main.gameMenu)
+			{
+				Main.time = 16200;
+				Main.dayTime = false;
+			}
+			if (++tick >= tickMax)
+			{
+				tick = 0;
+				glintPercent = 0f;
+			}
+			if (tick > tickThresholdForGlint)
+			{
+				glintPercent = (float)(tick - tickThresholdForGlint) / (tickMax - tickThresholdForGlint);
+			}
+		}
+		
+		internal static readonly float constLogoScale = 0.8f;
 		public override bool PreDrawLogo(SpriteBatch spriteBatch, ref Vector2 logoDrawCenter, ref float logoRotation, ref float logoScale, ref Color drawColor)
 		{
 			logoRotation = 0f;
-			logoScale = 1f;
+			logoScale = constLogoScale;
 			drawColor = new(255, 255, 255);
 			return true;
 			//return base.PreDrawLogo(spriteBatch, ref logoDrawCenter, ref logoRotation, ref logoScale, ref drawColor);
 		}
-		public override void Update(bool isOnTitleScreen)
+		public override void PostDrawLogo(SpriteBatch spriteBatch, Vector2 logoDrawCenter, float logoRotation, float logoScale, Color drawColor)
 		{
-			if (isOnTitleScreen)
+			if (tick >= tickThresholdForGlint)
 			{
-				//Main.SkipToTime(16200, false);
-				Main.time = 16200;
-				Main.dayTime = false;
+				if (tick % 5 > 0)
+				{
+					Vector2 glint1val = new(546 * (1f - glintPercent) - Logo.Width() / 2 - LogoGlint.Width() / 2, 136 - Logo.Height() / 2 - LogoGlint.Height() / 2);
+					spriteBatch.Draw(LogoGlint.Value, logoDrawCenter + glint1val * constLogoScale, new(0,0,14,14), drawColor, (float)(Math.PI * 0.25f), new(0.5f,0.5f), 0.8f, SpriteEffects.None, 0);
+				}
+				if (tick % 6 > 0)
+				{
+					Vector2 glint2val = new(546 * (glintPercent) - Logo.Width() / 2 - LogoGlint.Width() / 2, 0 - Logo.Height() / 2 - LogoGlint.Height() / 2);
+					spriteBatch.Draw(LogoGlint.Value, logoDrawCenter + glint2val * constLogoScale, new(0,0,14,14), drawColor, (float)(Math.PI * 0.25f), new(0.5f,0.5f), 0.8f, SpriteEffects.None, 0);
+				}
 			}
-			//Main.time = 16200;
-			//Main.dayTime = false;
 		}
 	}
 }
